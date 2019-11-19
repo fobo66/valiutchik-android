2f214d347a83d7addbd202b297be594760102545
Andrey Mukamolov <fobo66@protonmail.com>
2019-11-19 22:13:13 +0300

    Added tests to old code converter

---
+++ app/src/test/java/fobo66/exchangecourcesbelarus/entities/BestCourseTest.kt
@@ -0,0 +1,38 @@
+package fobo66.exchangecourcesbelarus.entities
+
+import fobo66.exchangecourcesbelarus.util.Constants
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+/**
+ * (c) 2019 Andrey Mukamolov <fobo66@protonmail.com>
+ * Created 11/19/19.
+ */
+class BestCourseTest {
+
+  private val bestBuyCourse = BestCourse("test", "1.925", Constants.USD, true)
+  private val bestSellCourse = BestCourse("test", "1.925", Constants.USD, false)
+
+  @Test
+  fun toBuyRateCorrectConversion() {
+    val buyRate = bestBuyCourse.toBestBuyRate()
+    assertEquals(bestBuyCourse.currencyValue, buyRate.currencyValue)
+  }
+
+  @Test(expected = IllegalStateException::class)
+  fun toBuyRateIncorrectConversion() {
+    bestSellCourse.toBestBuyRate()
+  }
+
+  @Test
+  fun toSellRateCorrectConversion() {
+    val sellRate = bestSellCourse.toBestSellRate()
+    assertEquals(bestSellCourse.currencyValue, sellRate.currencyValue)
+  }
+
+  @Test(expected = IllegalStateException::class)
+  fun toSellRateIncorrectConversion() {
+    bestBuyCourse.toBestSellRate()
+  }
+
+}
