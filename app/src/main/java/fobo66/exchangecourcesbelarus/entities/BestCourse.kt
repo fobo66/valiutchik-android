ef0b8b9e5ce43dd3381dbd86431adae7b8c7ea76
Andrey Mukamolov <fobo66@protonmail.com>
2019-11-26 12:43:16 +0300

    Fixed lint error

--- app/src/main/java/fobo66/exchangecourcesbelarus/entities/BestCourse.kt
+++ app/src/main/java/fobo66/exchangecourcesbelarus/entities/BestCourse.kt
@@ -3,6 +3,7 @@ package fobo66.exchangecourcesbelarus.entities
 import android.os.Parcelable
 import com.google.firebase.database.IgnoreExtraProperties
 import fobo66.exchangecourcesbelarus.util.CurrencyName
+import fobo66.exchangecourcesbelarus.util.USD
 import kotlinx.android.parcel.Parcelize
 
 /**
@@ -14,6 +15,6 @@ import kotlinx.android.parcel.Parcelize
 data class BestCourse(
   val bank: String = "",
   val currencyValue: String = "",
-  @CurrencyName val currencyName: String = "",
+  @CurrencyName val currencyName: String = USD,
   val isBuy: Boolean = false
 ) : Parcelable
