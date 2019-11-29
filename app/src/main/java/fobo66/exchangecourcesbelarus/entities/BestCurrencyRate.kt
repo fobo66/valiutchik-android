32e2854ea6d85453647f86d3c386b1259e67ae9a
Andrey Mukamolov <fobo66@protonmail.com>
2019-11-29 17:11:18 +0300

    Cleaned up UI entity

--- app/src/main/java/fobo66/exchangecourcesbelarus/entities/BestCurrencyRate.kt
+++ app/src/main/java/fobo66/exchangecourcesbelarus/entities/BestCurrencyRate.kt
@@ -1,40 +1,17 @@
 package fobo66.exchangecourcesbelarus.entities
 
-import fobo66.exchangecourcesbelarus.entities.BestCurrencyRate.BestBuyRate
-import fobo66.exchangecourcesbelarus.entities.BestCurrencyRate.BestSellRate
 import fobo66.exchangecourcesbelarus.util.CurrencyName
 
 /**
  * (c) 2019 Andrey Mukamolov <fobo66@protonmail.com>
  * Created 11/17/19.
  */
-sealed class BestCurrencyRate(
-  open var id: Int,
-  open var bank: String,
-  @CurrencyName open var currencyName: String,
-  open var currencyValue: String
-) {
-  class BestBuyRate(
-    val buyId: Int,
-    override var bank: String,
-    @CurrencyName override var currencyName: String,
-    override var currencyValue: String
-  ) : BestCurrencyRate(buyId, bank, currencyName, currencyValue)
+data class BestCurrencyRate(
+  val id: Long,
+  val bank: String,
+  @CurrencyName val currencyName: String,
+  val currencyValue: String
+)
 
-  class BestSellRate(
-    val sellId: Int,
-    override var bank: String,
-    @CurrencyName override var currencyName: String,
-    override var currencyValue: String
-  ) : BestCurrencyRate(sellId, bank, currencyName, currencyValue)
-}
-
-fun BestCourse.toBestBuyRate(): BestBuyRate {
-  check(isBuy) { "Wrong value. Expected it to be buy course, but was sell course" }
-  return BestBuyRate(0, bank, currencyName, currencyValue)
-}
-
-fun BestCourse.toBestSellRate(): BestSellRate {
-  check(!isBuy) { "Wrong value. Expected it to be sell course, but was buy course" }
-  return BestSellRate(0, bank, currencyName, currencyValue)
-}
+fun BestCourse.toBestCurrencyRate(): BestCurrencyRate =
+  BestCurrencyRate(id, bank, currencyName, currencyValue)
