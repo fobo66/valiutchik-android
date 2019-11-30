4a42158026344f8f65caa913a9d4cb32300100b6
Andrey Mukamolov <fobo66@protonmail.com>
2019-11-30 13:07:47 +0300

    Added initial test for repo

---
+++ app/src/test/java/fobo66/exchangecourcesbelarus/model/repository/CurrencyRateRepositoryTest.kt
@@ -0,0 +1,24 @@
+package fobo66.exchangecourcesbelarus.model.repository
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+/**
+ * (c) 2019 Andrey Mukamolov <fobo66@protonmail.com>
+ * Created 11/30/19.
+ */
+class CurrencyRateRepositoryTest {
+
+  @Before
+  fun setUp() {
+  }
+
+  @After
+  fun tearDown() {
+  }
+
+  @Test
+  fun loadExchangeRates() {
+  }
+}
