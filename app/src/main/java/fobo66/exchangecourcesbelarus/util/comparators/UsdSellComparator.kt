a7310b0c99f98631995790dbecf0df565ea53402
Andrey Mukamolov <amv@bamboogroup.eu>
2020-03-06 13:02:10 +0300

    Cleanup unused code

--- app/src/main/java/fobo66/exchangecourcesbelarus/util/comparators/UsdSellComparator.kt
+++
@@ -1,9 +0,0 @@
-package fobo66.exchangecourcesbelarus.util.comparators
-
-import fobo66.exchangecourcesbelarus.entities.Currency
-
-class UsdSellComparator : Comparator<Currency> {
-  override fun compare(lhs: Currency, rhs: Currency): Int {
-    return rhs.usdSell.compareTo(lhs.usdSell, ignoreCase = true)
-  }
-}
