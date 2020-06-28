12c0457cdb0f1d1119a5144c72697fd494c56e0c
Andrey Mukamolov <fobo66@protonmail.com>
2020-06-28 12:19:26 +0300

    Fixed instrumented test

--- app/src/androidTest/java/fobo66/exchangecourcesbelarus/util/CurrencyEvaluatorTest.kt
+++ app/src/androidTest/java/fobo66/exchangecourcesbelarus/util/CurrencyEvaluatorTest.kt
@@ -5,8 +5,8 @@ import fobo66.exchangecourcesbelarus.model.MyfinParser
 import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
-import org.threeten.bp.LocalDateTime
 import java.io.InputStream
+import java.time.LocalDateTime
 
 /**
  * (c) 2017 Andrey Mukamolov aka fobo66 <fobo66@protonmail.com>
@@ -21,7 +21,6 @@ class CurrencyEvaluatorTest {
   private val timestamp = LocalDateTime.now().toString()
 
   @Before
-  @Throws(Exception::class)
   fun setUp() {
     evaluator = CurrencyEvaluator(CurrencyListSanitizerImpl())
     testFile = javaClass.classLoader?.getResourceAsStream("data.xml")!!
@@ -32,42 +31,36 @@ class CurrencyEvaluatorTest {
   }
 
   @Test
-  @Throws(Exception::class)
   fun testBestUSDBuyCoursesAreReallyBest() {
     assertEquals(USD, bestBuy[USD_INDEX].currencyName)
     assertEquals("1.925", bestBuy[USD_INDEX].currencyValue)
   }
 
   @Test
-  @Throws(Exception::class)
   fun testBestRURBuyCourseAreReallyBest() {
     assertEquals(RUR, bestBuy[RUR_INDEX].currencyName)
     assertEquals("0.0324", bestBuy[RUR_INDEX].currencyValue)
   }
 
   @Test
-  @Throws(Exception::class)
   fun testBestEURBuyCourseAreReallyBest() {
     assertEquals(EUR, bestBuy[EUR_INDEX].currencyName)
     assertEquals("2.075", bestBuy[EUR_INDEX].currencyValue)
   }
 
   @Test
-  @Throws(Exception::class)
   fun testBestUSDSellCoursesAreReallyBest() {
     assertEquals(USD, bestSell[USD_INDEX].currencyName)
     assertEquals("1.914", bestSell[USD_INDEX].currencyValue)
   }
 
   @Test
-  @Throws(Exception::class)
   fun testBestRURSellCourseAreReallyBest() {
     assertEquals(RUR, bestSell[RUR_INDEX].currencyName)
     assertEquals("0.0323", bestSell[RUR_INDEX].currencyValue)
   }
 
   @Test
-  @Throws(Exception::class)
   fun testBestEURSellCourseAreReallyBest() {
     assertEquals(EUR, bestSell[EUR_INDEX].currencyName)
     assertEquals("2.038", bestSell[EUR_INDEX].currencyValue)
