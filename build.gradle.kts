f6f502d27bdd8c5b2899f9b09a071ac2b546e638
Andrey Mukamolov <amv@bamboogroup.eu>
2020-10-09 16:59:48 +0300

    Updated AGP

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
     google()
   }
   dependencies {
-    classpath("com.android.tools.build:gradle:4.2.0-alpha12")
+    classpath("com.android.tools.build:gradle:4.2.0-alpha13")
     classpath("com.google.gms:google-services:4.3.3")
     classpath("com.google.firebase:firebase-crashlytics-gradle:2.3.0")
     classpath(kotlin("gradle-plugin", version = "1.4.10"))
