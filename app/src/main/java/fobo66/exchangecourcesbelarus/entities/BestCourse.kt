cb121b9e9769973841fe1cda64fb25d00af83496
Andrey Mukamolov <amv@bamboogroup.eu>
2020-10-09 17:00:11 +0300

    Removed Parcelable implementation completely

--- app/src/main/java/fobo66/exchangecourcesbelarus/entities/BestCourse.kt
+++ app/src/main/java/fobo66/exchangecourcesbelarus/entities/BestCourse.kt
@@ -1,8 +1,5 @@
 package fobo66.exchangecourcesbelarus.entities
 
-import android.os.Parcel
-import android.os.Parcelable
-import android.os.Parcelable.Creator
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
@@ -21,39 +18,4 @@ data class BestCourse(
   @ColumnInfo(name = "currency_name") @CurrencyName val currencyName: String = USD,
   @ColumnInfo(name = "timestamp") val timestamp: String = "",
   @ColumnInfo(name = "is_buy") val isBuy: Boolean = false
-) : Parcelable {
-  constructor(parcel: Parcel) : this(
-    parcel.readLong(),
-    parcel.readString().orEmpty(),
-    parcel.readString().orEmpty(),
-    parcel.readString().orEmpty(),
-    parcel.readString().orEmpty(),
-    parcel.readByte() != 0.toByte()
-  )
-
-  fun toBestCurrencyRate(): BestCurrencyRate =
-    BestCurrencyRate(id, bank, currencyName, currencyValue)
-
-  override fun writeToParcel(parcel: Parcel, flags: Int) {
-    parcel.writeLong(id)
-    parcel.writeString(bank)
-    parcel.writeString(currencyValue)
-    parcel.writeString(currencyName)
-    parcel.writeString(timestamp)
-    parcel.writeByte(if (isBuy) 1 else 0)
-  }
-
-  override fun describeContents(): Int {
-    return 0
-  }
-
-  companion object CREATOR : Creator<BestCourse> {
-    override fun createFromParcel(parcel: Parcel): BestCourse {
-      return BestCourse(parcel)
-    }
-
-    override fun newArray(size: Int): Array<BestCourse?> {
-      return arrayOfNulls(size)
-    }
-  }
-}
+)
