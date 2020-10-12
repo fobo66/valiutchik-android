f4f3a924e4ca21365b4177fede9aef3dd81d4ebc
Andrey Mukamolov <amv@bamboogroup.eu>
2020-10-12 13:15:50 +0300

    Prepare to extract timestamp logic

---
+++ app/src/main/java/fobo66/exchangecourcesbelarus/model/repository/CurrencyRatesTimestampRepository.kt
@@ -0,0 +1,38 @@
+package fobo66.exchangecourcesbelarus.model.repository
+
+import fobo66.exchangecourcesbelarus.model.datasource.PreferencesDataSource
+import fobo66.exchangecourcesbelarus.util.TIMESTAMP
+import java.time.Duration
+import java.time.LocalDateTime
+import javax.inject.Inject
+
+class CurrencyRatesTimestampRepository @Inject constructor(
+  private val preferencesDataSource: PreferencesDataSource
+) {
+
+  private val maxStalePeriod: Duration = Duration.ofHours(3)
+
+
+  fun isNeededToUpdateCurrencyRates(now: LocalDateTime): Boolean {
+    val timestamp = loadTimestamp(now)
+    val cachedValueAge = Duration.between(timestamp, now)
+
+    return cachedValueAge == Duration.ZERO || cachedValueAge > maxStalePeriod
+  }
+
+  fun saveTimestamp(now: LocalDateTime) {
+    val nowString = now.toString()
+    preferencesDataSource.saveString(TIMESTAMP, nowString)
+  }
+
+  private fun loadTimestamp(fallbackTimestamp: LocalDateTime): LocalDateTime {
+    val rawTimestamp: String = preferencesDataSource.loadSting(TIMESTAMP)
+
+    return if (rawTimestamp.isEmpty()) {
+      fallbackTimestamp
+    } else {
+      LocalDateTime.parse(rawTimestamp)
+    }
+  }
+
+}
