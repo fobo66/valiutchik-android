24e7306d7d1fcc23369762308e07a2f555b3cb9e
Andrey Mukamolov <fobo66@protonmail.com>
2020-10-15 18:11:18 +0300

    Extracted timestamp logic to separate repo

--- app/src/main/java/fobo66/exchangecourcesbelarus/model/RefreshExchangeRates.kt
+++ app/src/main/java/fobo66/exchangecourcesbelarus/model/RefreshExchangeRates.kt
@@ -1,20 +1,25 @@
 package fobo66.exchangecourcesbelarus.model
 
 import fobo66.exchangecourcesbelarus.model.repository.CurrencyRateRepository
+import fobo66.exchangecourcesbelarus.model.repository.CurrencyRatesTimestampRepository
 import fobo66.exchangecourcesbelarus.model.repository.LocationRepository
 import java.time.LocalDateTime
 import javax.inject.Inject
 
 interface RefreshExchangeRates {
-  suspend fun execute(latitude: Double, longitude: Double)
+  suspend fun execute(latitude: Double, longitude: Double, now: LocalDateTime)
 }
 
 class RefreshExchangeRatesImpl @Inject constructor(
   private val locationRepository: LocationRepository,
+  private val timestampRepository: CurrencyRatesTimestampRepository,
   private val currencyRateRepository: CurrencyRateRepository
 ) : RefreshExchangeRates {
-  override suspend fun execute(latitude: Double, longitude: Double) {
+  override suspend fun execute(latitude: Double, longitude: Double, now: LocalDateTime) {
     val city = locationRepository.resolveUserCity(latitude, longitude)
-    currencyRateRepository.refreshExchangeRates(city, LocalDateTime.now())
+
+    if (timestampRepository.isNeededToUpdateCurrencyRates(now)) {
+      currencyRateRepository.refreshExchangeRates(city, now)
+    }
   }
 }
--- app/src/main/java/fobo66/exchangecourcesbelarus/model/repository/CurrencyRateRepository.kt
+++ app/src/main/java/fobo66/exchangecourcesbelarus/model/repository/CurrencyRateRepository.kt
@@ -6,14 +6,11 @@ import fobo66.exchangecourcesbelarus.entities.Currency
 import fobo66.exchangecourcesbelarus.model.CurrencyRatesParser
 import fobo66.exchangecourcesbelarus.model.datasource.CurrencyRatesDataSource
 import fobo66.exchangecourcesbelarus.model.datasource.PersistenceDataSource
-import fobo66.exchangecourcesbelarus.model.datasource.PreferencesDataSource
 import fobo66.exchangecourcesbelarus.util.CurrencyEvaluator
-import fobo66.exchangecourcesbelarus.util.TIMESTAMP
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.withContext
 import timber.log.Timber
-import java.time.Duration
 import java.time.LocalDateTime
 import javax.inject.Inject
 
@@ -25,34 +22,24 @@ class CurrencyRateRepository @Inject constructor(
   private val parser: CurrencyRatesParser,
   private val currencyEvaluator: CurrencyEvaluator,
   private val persistenceDataSource: PersistenceDataSource,
-  private val preferencesDataSource: PreferencesDataSource,
   private val currencyRatesDataSource: CurrencyRatesDataSource,
   @Io private val ioDispatcher: CoroutineDispatcher
 ) {
 
-  private val maxStalePeriod: Duration = Duration.ofHours(3)
-
   suspend fun refreshExchangeRates(city: String, now: LocalDateTime) {
-    val timestamp = loadTimestamp(now)
+    val currenciesResponse = try {
+      currencyRatesDataSource.loadExchangeRates(city)
+    } catch (e: Exception) {
+      Timber.e(e, "Failed to load exchange rates")
+      null
+    }
 
-    if (needToUpdateCurrencyRates(Duration.between(timestamp, now))) {
-      val currenciesResponse = try {
-        currencyRatesDataSource.loadExchangeRates(city)
-      } catch (e: Exception) {
-        Timber.e(e, "Failed to load exchange rates")
-        null
-      }
+    currenciesResponse?.body?.let {
+      withContext(ioDispatcher) {
+        val currencies = parser.parse(it.byteStream())
+        val bestCourses = findBestCourses(currencies, now.toString())
 
-      currenciesResponse?.body?.let {
-        withContext(ioDispatcher) {
-          val nowString = now.toString()
-          preferencesDataSource.saveString(TIMESTAMP, nowString)
-          val currencies = parser.parse(it.byteStream())
-
-          val bestCourses = findBestCourses(currencies, nowString)
-
-          persistenceDataSource.saveBestCourses(bestCourses)
-        }
+        persistenceDataSource.saveBestCourses(bestCourses)
       }
     }
   }
@@ -69,17 +56,4 @@ class CurrencyRateRepository @Inject constructor(
     bestCourses.addAll(currencyEvaluator.findBestSellCourses(currencies, now))
     return bestCourses
   }
-
-  private fun loadTimestamp(fallbackTimestamp: LocalDateTime): LocalDateTime {
-    val rawTimestamp: String = preferencesDataSource.loadSting(TIMESTAMP)
-
-    return if (rawTimestamp.isEmpty()) {
-      fallbackTimestamp
-    } else {
-      LocalDateTime.parse(rawTimestamp)
-    }
-  }
-
-  private fun needToUpdateCurrencyRates(cachedValueAge: Duration): Boolean =
-    cachedValueAge == Duration.ZERO || cachedValueAge > maxStalePeriod
 }
