b0816595f025c905cf035925c81ef596f5736745
Andrey Mukamolov <fobo66@protonmail.com>
2020-11-06 01:03:45 +0300

    Convert core module buildscript to Gradle Kotlin DSL

---
+++ core/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+  id("com.android.library")
+  kotlin("android")
+}
+
+android {
+  compileSdk = 30
+
+  defaultConfig {
+    minSdk = 16
+    targetSdk = 30
+    versionCode = 1
+    versionName = "1.0"
+
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    consumerProguardFiles("consumer-rules.pro")
+  }
+
+  buildTypes {
+    getByName("release") {
+      isMinifyEnabled = false
+      proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+    }
+  }
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+  }
+  kotlinOptions {
+    jvmTarget = "1.8"
+  }
+}
+
+dependencies {
+  implementation("androidx.annotation:annotation:1.1.0")
+  implementation("javax.inject:javax.inject:1")
+  testImplementation("junit:junit:4.13.1")
+}
