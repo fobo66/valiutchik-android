1e788cca2931b4451fc64455afa06e54b578f059
Andrey Mukamolov <fobo66@protonmail.com>
2020-11-12 23:53:04 +0300

    updated deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -99,7 +99,7 @@ dependencies {
   implementation("androidx.recyclerview:recyclerview:1.1.0")
   implementation("androidx.fragment:fragment-ktx:1.3.0-beta01")
   implementation("androidx.collection:collection-ktx:1.1.0")
-  implementation("androidx.core:core-ktx:1.5.0-alpha04")
+  implementation("androidx.core:core-ktx:1.5.0-alpha05")
   implementation("androidx.constraintlayout:constraintlayout:2.0.4")
   implementation("com.google.android.material:material:1.3.0-alpha03")
   implementation("androidx.preference:preference-ktx:1.1.1")
@@ -140,7 +140,7 @@ dependencies {
   // leakcanary
   debugImplementation("com.squareup.leakcanary:leakcanary-android:2.4")
 
-  coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:1.1.0")
+  coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:1.1.1")
 
   // tests
   testImplementation("junit:junit:4.13.1")
