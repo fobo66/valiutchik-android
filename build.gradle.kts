85b6048757499e30211f77e508805e1210322b01
Andrey Mukamolov <fobo66@protonmail.com>
2020-11-24 19:15:49 +0300

    Updated deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,8 +8,8 @@ buildscript {
   dependencies {
     classpath("com.android.tools.build:gradle:4.2.0-alpha16")
     classpath("com.google.gms:google-services:4.3.4")
-    classpath("com.google.firebase:firebase-crashlytics-gradle:2.3.0")
-    classpath(kotlin("gradle-plugin", version = "1.4.10"))
+    classpath("com.google.firebase:firebase-crashlytics-gradle:2.4.1")
+    classpath(kotlin("gradle-plugin", version = "1.4.20"))
     classpath("androidx.benchmark:benchmark-gradle-plugin:1.0.0")
   }
 }
