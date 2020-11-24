2b4de07a4218af06276bbfcbceb1bde00ad7f879
Andrey Mukamolov <fobo66@protonmail.com>
2020-11-25 02:39:05 +0300

    Added extension for Currency entity for nicer code

--- app/src/main/java/fobo66/exchangecourcesbelarus/util/BestCourseProducer.kt
+++ app/src/main/java/fobo66/exchangecourcesbelarus/util/BestCourseProducer.kt
@@ -2,7 +2,6 @@ package fobo66.exchangecourcesbelarus.util
 
 import fobo66.exchangecourcesbelarus.entities.BestCourse
 import fobo66.valiutchik.core.BUY_COURSE
-import fobo66.valiutchik.core.CurrencyName
 import fobo66.valiutchik.core.EUR
 import fobo66.valiutchik.core.RUR
 import fobo66.valiutchik.core.SELL_COURSE
@@ -33,14 +32,14 @@ class BestCourseProducer @Inject constructor() {
     currencyKeys.forEach { currencyKey ->
       val currency = courses.asSequence()
         .filter { isBuyRateCorrect(it, currencyKey) }
-        .maxByOrNull { resolveCurrencyBuyRate(it, currencyKey) } ?: courses.first {
+        .maxByOrNull { it.resolveCurrencyBuyRate(currencyKey) } ?: courses.first {
         isBuyRateCorrect(it, currencyKey)
       }
       result.add(
         BestCourse(
           0L,
           escapeBankName(currency.bankname),
-          resolveCurrencyBuyRate(currency, currencyKey),
+          currency.resolveCurrencyBuyRate(currencyKey),
           currencyKey,
           timestamp,
           BUY_COURSE
@@ -59,14 +58,14 @@ class BestCourseProducer @Inject constructor() {
     currencyKeys.forEach { currencyKey ->
       val currency = courses.asSequence()
         .filter { isSellRateCorrect(it, currencyKey) }
-        .minByOrNull { resolveCurrencySellRate(it, currencyKey) } ?: courses.first {
+        .minByOrNull { it.resolveCurrencySellRate(currencyKey) } ?: courses.first {
         isSellRateCorrect(it, currencyKey)
       }
       result.add(
         BestCourse(
           0L,
           escapeBankName(currency.bankname),
-          resolveCurrencySellRate(currency, currencyKey),
+          currency.resolveCurrencySellRate(currencyKey),
           currencyKey,
           timestamp,
           SELL_COURSE
@@ -79,41 +78,15 @@ class BestCourseProducer @Inject constructor() {
   private fun isSellRateCorrect(
     currency: Currency,
     currencyKey: String
-  ) = resolveCurrencySellRate(currency, currencyKey) != UNKNOWN_COURSE
+  ) = currency.resolveCurrencySellRate(currencyKey) != UNKNOWN_COURSE
 
   private fun isBuyRateCorrect(
     currency: Currency,
     currencyKey: String
-  ) = resolveCurrencyBuyRate(currency, currencyKey) != UNKNOWN_COURSE
+  ) = currency.resolveCurrencyBuyRate(currencyKey) != UNKNOWN_COURSE
 
   private fun escapeBankName(bankName: String): String {
     val matcher = bankNamePattern.matcher(bankName)
     return if (matcher.find()) matcher.group(0) ?: bankName else bankName
-  }
-
-  /**
-   * Method to figure out which currency will be used depends on the context
-   * By default, USD value is returned
-   * If I find the better way to do it, I'll rewrite it.
-   */
-  private fun resolveCurrencyBuyRate(currency: Currency, @CurrencyName name: String): String {
-    return when (name) {
-      EUR -> currency.eurBuy
-      RUR -> currency.rurBuy
-      USD -> currency.usdBuy
-      else -> currency.usdBuy
-    }
-  }
-
-  /**
-   * See above.
-   */
-  private fun resolveCurrencySellRate(currency: Currency, @CurrencyName name: String): String {
-    return when (name) {
-      EUR -> currency.eurSell
-      RUR -> currency.rurSell
-      USD -> currency.usdSell
-      else -> currency.usdSell
-    }
   }
 }
---
+++ app/src/main/java/fobo66/exchangecourcesbelarus/util/CurrencyExtension.kt
@@ -0,0 +1,33 @@
+package fobo66.exchangecourcesbelarus.util
+
+import fobo66.valiutchik.core.CurrencyName
+import fobo66.valiutchik.core.EUR
+import fobo66.valiutchik.core.RUR
+import fobo66.valiutchik.core.USD
+import fobo66.valiutchik.core.entities.Currency
+
+/**
+ * Method to figure out which currency will be used depends on the context
+ * By default, USD value is returned
+ * If I find the better way to do it, I'll rewrite it.
+ */
+fun Currency.resolveCurrencyBuyRate(@CurrencyName name: String): String {
+  return when (name) {
+    EUR -> eurBuy
+    RUR -> rurBuy
+    USD -> usdBuy
+    else -> usdBuy
+  }
+}
+
+/**
+ * See above.
+ */
+fun Currency.resolveCurrencySellRate(@CurrencyName name: String): String {
+  return when (name) {
+    EUR -> eurSell
+    RUR -> rurSell
+    USD -> usdSell
+    else -> usdSell
+  }
+}
