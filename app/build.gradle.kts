ab4e5df4063c3e982007105e4cffbf27b6129f27
Andrey Mukamolov <fobo66@protonmail.com>
2021-01-07 05:03:10 +0300

    Prepared next development iteration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
     applicationId = "fobo66.exchangecourcesbelarus"
     minSdk = 16
     targetSdk = 30
-    versionCode = 15
-    versionName = "1.11"
+    versionCode = 16
+    versionName = "1.11.1"
     multiDexEnabled = true
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
