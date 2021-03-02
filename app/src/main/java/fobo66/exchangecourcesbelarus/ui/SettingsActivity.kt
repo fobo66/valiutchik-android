3f00b5069bf86f0f70f785f8ccce3cc5ae441835
Andrey Mukamolov <fobo66@protonmail.com>
2021-03-03 02:32:38 +0300

    Cleaned up SettingsActivity

--- app/src/main/java/fobo66/exchangecourcesbelarus/ui/SettingsActivity.kt
+++ app/src/main/java/fobo66/exchangecourcesbelarus/ui/SettingsActivity.kt
@@ -6,9 +6,13 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.app.ActivityOptionsCompat
+import androidx.lifecycle.lifecycleScope
 import dev.chrisbanes.insetter.Insetter
 import dev.chrisbanes.insetter.Side
 import fobo66.exchangecourcesbelarus.databinding.ActivitySettingsBinding
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import reactivecircus.flowbinding.appcompat.navigationClicks
 
 class SettingsActivity : AppCompatActivity() {
 
@@ -18,16 +22,18 @@ class SettingsActivity : AppCompatActivity() {
     super.onCreate(savedInstanceState)
     binding = ActivitySettingsBinding.inflate(layoutInflater)
     setContentView(binding.root)
-    setupActionBar()
-    Insetter.builder().applySystemWindowInsetsToMargin(Side.TOP).applyToView(binding.toolbar)
+    setupToolbar()
   }
 
-  private fun setupActionBar() {
+  private fun setupToolbar() {
+    Insetter.builder().applySystemWindowInsetsToMargin(Side.TOP).applyToView(binding.toolbar)
+
     Insetter.builder().applySystemWindowInsetsToPadding(Side.RIGHT or Side.LEFT)
       .applyToView(binding.appbar)
-    binding.toolbar.setNavigationOnClickListener {
-      onBackPressedDispatcher.onBackPressed()
-    }
+
+    binding.toolbar.navigationClicks()
+      .onEach { onBackPressedDispatcher.onBackPressed() }
+      .launchIn(lifecycleScope)
   }
 
   companion object {
