f6b241a1f050be49d7f8a32fc1529489a7e889ec
Andrey Mukamolov <fobo66@protonmail.com>
2021-04-17 14:58:16 +0300

    Move MainFragment to appropriate package

--- app/src/main/java/fobo66/exchangecourcesbelarus/MainFragment.kt
+++
@@ -1,8 +0,0 @@
-package fobo66.exchangecourcesbelarus
-
-import androidx.fragment.app.Fragment
-
-/**
- * Main fragment with the list of currency rates
- */
-class MainFragment : Fragment(R.layout.fragment_main)
---
+++ app/src/main/java/fobo66/exchangecourcesbelarus/ui/MainFragment.kt
@@ -0,0 +1,9 @@
+package fobo66.exchangecourcesbelarus.ui
+
+import androidx.fragment.app.Fragment
+import fobo66.exchangecourcesbelarus.R.layout
+
+/**
+ * Main fragment with the list of currency rates
+ */
+class MainFragment : Fragment(layout.fragment_main)
