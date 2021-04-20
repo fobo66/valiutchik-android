3552bc6727b64a7fb5a289a461609268c4547483
Andrey Mukamolov <fobo66@protonmail.com>
2021-04-21 02:55:23 +0300

    Fixed UI test

--- app/src/androidTest/java/fobo66/exchangecourcesbelarus/ui/SettingsScreen.kt
+++ app/src/androidTest/java/fobo66/exchangecourcesbelarus/ui/SettingsScreen.kt
@@ -3,14 +3,12 @@ package fobo66.exchangecourcesbelarus.ui
 import com.agoda.kakao.common.views.KView
 import com.kaspersky.kaspresso.screens.KScreen
 import fobo66.exchangecourcesbelarus.R
-import fobo66.exchangecourcesbelarus.R.id
 
 object SettingsScreen : KScreen<SettingsScreen>() {
   val settings = KView {
-    withId(id.settings_container)
-
+    withId(android.R.id.list_container)
   }
 
-  override val layoutId = R.layout.activity_settings
-  override val viewClass = SettingsActivity::class.java
+  override val layoutId = R.layout.activity_main
+  override val viewClass = MainActivity::class.java
 }
