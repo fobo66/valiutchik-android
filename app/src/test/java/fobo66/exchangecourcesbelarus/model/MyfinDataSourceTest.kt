9fe417808724c73a34c29cfb58a84f80e66765ea
Andrey Mukamolov <fobo66@protonmail.com>
2021-04-28 03:16:48 +0300

    Use MockWebServerRule

--- app/src/test/java/fobo66/exchangecourcesbelarus/model/MyfinDataSourceTest.kt
+++ app/src/test/java/fobo66/exchangecourcesbelarus/model/MyfinDataSourceTest.kt
@@ -5,14 +5,14 @@ import fobo66.exchangecourcesbelarus.model.datasource.MyfinDataSource
 import fobo66.valiutchik.core.BASE_URL
 import kotlinx.coroutines.runBlocking
 import mockwebserver3.MockResponse
-import mockwebserver3.MockWebServer
+import mockwebserver3.junit4.MockWebServerRule
 import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.OkHttpClient
-import org.junit.After
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import javax.net.ssl.HttpsURLConnection
 
@@ -22,26 +22,23 @@ import javax.net.ssl.HttpsURLConnection
  */
 class MyfinDataSourceTest {
 
-  private lateinit var mockWebServer: MockWebServer
+  @JvmField
+  @Rule
+  val serverRule = MockWebServerRule()
+
   private lateinit var dataSource: CurrencyRatesDataSource
 
   @Before
   fun setUp() {
-    mockWebServer = MockWebServer()
     dataSource = MyfinDataSource(
       OkHttpClient(),
-      mockWebServer.url("").toString()
+      serverRule.server.url("").toString()
     )
-  }
-
-  @After
-  fun tearDown() {
-    mockWebServer.close()
   }
 
   @Test
   fun `load exchange rates`() {
-    mockWebServer.enqueue(MockResponse())
+    serverRule.server.enqueue(MockResponse())
 
     runBlocking {
       val response = dataSource.loadExchangeRates("Минск")
@@ -51,7 +48,7 @@ class MyfinDataSourceTest {
 
   @Test
   fun `correct url for city`() {
-    mockWebServer.enqueue(MockResponse())
+    serverRule.server.enqueue(MockResponse())
 
     runBlocking {
       val response = dataSource.loadExchangeRates("Могилёв")
@@ -61,7 +58,7 @@ class MyfinDataSourceTest {
 
   @Test
   fun `default url for unknown city`() {
-    mockWebServer.enqueue(MockResponse())
+    serverRule.server.enqueue(MockResponse())
 
     runBlocking {
       val response = dataSource.loadExchangeRates("test")
@@ -71,7 +68,7 @@ class MyfinDataSourceTest {
 
   @Test
   fun `error while loading exchange rates`() {
-    mockWebServer.enqueue(MockResponse().setResponseCode(HttpsURLConnection.HTTP_INTERNAL_ERROR))
+    serverRule.server.enqueue(MockResponse().setResponseCode(HttpsURLConnection.HTTP_INTERNAL_ERROR))
 
     runBlocking {
       val response = dataSource.loadExchangeRates("Минск")
