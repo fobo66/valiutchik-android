474b2e6e9ac698f4eee512a93e117d6f970c071a
Andrey Mukamolov <fobo66@protonmail.com>
2021-05-06 23:31:42 +0300

    Updated Kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
   }
   dependencies {
     classpath("com.android.tools.build:gradle:7.0.0-alpha15")
-    classpath(kotlin("gradle-plugin", version = "1.4.32"))
+    classpath(kotlin("gradle-plugin", version = "1.5.0"))
     classpath("androidx.benchmark:benchmark-gradle-plugin:1.0.0")
     classpath("com.google.dagger:hilt-android-gradle-plugin:2.35.1")
   }
