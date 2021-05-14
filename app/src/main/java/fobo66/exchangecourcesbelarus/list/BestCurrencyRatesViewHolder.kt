e9ae09bfaeb17c2575f72be5009f4ef0f30763a8
Andrey Mukamolov <amv@bamboogroup.eu>
2021-05-14 13:51:38 +0300

    Revert "WIP: show buy and sell rates on the one card"
    This reverts commit a027c2c9

--- app/src/main/java/fobo66/exchangecourcesbelarus/list/BestCurrencyRatesViewHolder.kt
+++ app/src/main/java/fobo66/exchangecourcesbelarus/list/BestCurrencyRatesViewHolder.kt
@@ -30,15 +30,13 @@ class BestCurrencyRatesViewHolder(private val binding: CurrencyCardBinding) :
 
   fun bind(item: BestCurrencyRate) {
     binding.currencyName.text = item.currencyName
-    binding.currencyValueBuy.text = item.currencyValueBuy
-    binding.currencyValueSell.text = item.currencyValueSell
-    binding.bankNameBuy.text = item.bankBuy
-    binding.bankNameSell.text = item.bankSell
+    binding.currencyValue.text = item.currencyValue
+    binding.bankName.text = item.bank
   }
 
   override fun onClick(view: View) {
     val req =
-      Uri.parse("geo:0,0?q=${binding.bankNameBuy.text.toString().replace(' ', '+')}")
+      Uri.parse("geo:0,0?q=${binding.bankName.text.toString().replace(' ', '+')}")
     val mapIntent = Intent(Intent.ACTION_VIEW, req)
 
     if (mapIntent.resolveActivity(itemView.context.packageManager) != null) {
@@ -50,7 +48,7 @@ class BestCurrencyRatesViewHolder(private val binding: CurrencyCardBinding) :
   }
 
   override fun onLongClick(view: View): Boolean {
-    val clipData = ClipData.newPlainText(binding.currencyName.text, binding.currencyValueBuy.text)
+    val clipData = ClipData.newPlainText(binding.currencyName.text, binding.currencyValue.text)
     val clipboardManager = itemView.context.getSystemService<ClipboardManager>()
     clipboardManager?.setPrimaryClip(clipData)
     Snackbar.make(itemView, R.string.currency_value_copied, Snackbar.LENGTH_SHORT).show()
--- app/src/main/java/fobo66/exchangecourcesbelarus/model/LoadExchangeRates.kt
+++ app/src/main/java/fobo66/exchangecourcesbelarus/model/LoadExchangeRates.kt
@@ -1,6 +1,5 @@
 package fobo66.exchangecourcesbelarus.model
 
-import fobo66.exchangecourcesbelarus.entities.BestCourse
 import fobo66.exchangecourcesbelarus.model.repository.CurrencyRateRepository
 import fobo66.valiutchik.core.entities.BestCurrencyRate
 import kotlinx.coroutines.flow.Flow
@@ -12,23 +11,12 @@ interface LoadExchangeRates {
 }
 
 class LoadExchangeRatesImpl @Inject constructor(
-  private val currencyRateRepository: CurrencyRateRepository
+    private val currencyRateRepository: CurrencyRateRepository
 ) : LoadExchangeRates {
-  override fun execute(isBuy: Boolean): Flow<List<BestCurrencyRate>> =
-    currencyRateRepository.loadExchangeRates(isBuy)
+  override fun execute(isBuy: Boolean): Flow<List<BestCurrencyRate>> {
+    return currencyRateRepository.loadExchangeRates(isBuy)
       .map {
-        val buyRates: List<BestCourse> = it.filter { it.isBuy }
-        val sellRates: List<BestCourse> = it.filter { !it.isBuy }
-
-        buyRates.zip(sellRates) { buyRate, sellRate ->
-          BestCurrencyRate(
-            buyRate.id,
-            buyRate.bank,
-            sellRate.bank,
-            buyRate.currencyName,
-            buyRate.currencyValue,
-            sellRate.currencyValue
-          )
-        }
+        it.map { bestCourse -> bestCourse.toBestCurrencyRate() }
       }
+  }
 }
