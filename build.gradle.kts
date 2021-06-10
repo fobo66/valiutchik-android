b1ab0dd15b1cb1270a9dbe39cd1f992c3f6b681b
Andrey Mukamolov <fobo66@protonmail.com>
2021-06-11 02:16:45 +0300

    Updated Dagger

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ buildscript {
     classpath("com.android.tools.build:gradle:7.1.0-alpha02")
     classpath(kotlin("gradle-plugin", version = "1.5.10"))
     classpath("androidx.benchmark:benchmark-gradle-plugin:1.0.0")
-    classpath("com.google.dagger:hilt-android-gradle-plugin:2.36")
+    classpath("com.google.dagger:hilt-android-gradle-plugin:2.37")
     classpath("com.jaredsburrows:gradle-license-plugin:0.8.90")
     classpath("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.17.1")
   }
