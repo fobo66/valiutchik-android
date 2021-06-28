10fb0002daa81035c76145c5f344e624fc8d02d8
Andrey Mukamolov <amv@bamboogroup.eu>
2021-06-28 17:27:52 +0300

    Request current location from LocationManager

--- app/src/main/java/fobo66/exchangecourcesbelarus/util/LocationResolverOldSchoolImpl.kt
+++ app/src/main/java/fobo66/exchangecourcesbelarus/util/LocationResolverOldSchoolImpl.kt
@@ -2,28 +2,48 @@ package fobo66.exchangecourcesbelarus.util
 
 import android.Manifest
 import android.app.Activity
-import android.location.Location
 import android.location.LocationManager
 import androidx.annotation.RequiresPermission
 import androidx.core.content.getSystemService
 import androidx.core.location.LocationManagerCompat
+import androidx.core.os.CancellationSignal
+import kotlinx.coroutines.suspendCancellableCoroutine
+import java.util.concurrent.Executors
 import javax.inject.Inject
+import kotlin.coroutines.resume
 
 class LocationResolverOldSchoolImpl @Inject constructor() : LocationResolver {
   @RequiresPermission(Manifest.permission.ACCESS_COARSE_LOCATION)
-  override suspend fun resolveLocation(activity: Activity): Pair<Double, Double> {
-    val noLocation = 0.0 to 0.0
-    val locationManager = activity.getSystemService<LocationManager>() ?: return noLocation
+  override suspend fun resolveLocation(activity: Activity): Pair<Double, Double> =
+    suspendCancellableCoroutine { continuation ->
+      val noLocation = 0.0 to 0.0
+      val locationManager = activity.getSystemService<LocationManager>()
 
-    return if (LocationManagerCompat.isLocationEnabled(locationManager)) {
-      val location: Location? =
-        locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
+      if (locationManager == null) {
+        continuation.resume(noLocation)
+      }
 
-      location?.let {
-        it.latitude to it.longitude
-      } ?: noLocation
-    } else {
-      noLocation
+      if (LocationManagerCompat.isLocationEnabled(locationManager!!)) {
+        val cancellationSignal = CancellationSignal()
+        cancellationSignal.setOnCancelListener {
+          if (!continuation.isCancelled) {
+            continuation.cancel()
+          }
+        }
+
+        LocationManagerCompat.getCurrentLocation(
+          locationManager,
+          LocationManager.NETWORK_PROVIDER,
+          cancellationSignal,
+          Executors.newSingleThreadExecutor(),
+          { location ->
+            continuation.resume(location?.let {
+              it.latitude to it.longitude
+            } ?: noLocation)
+          }
+        )
+      } else {
+        continuation.resume(noLocation)
+      }
     }
-  }
 }
