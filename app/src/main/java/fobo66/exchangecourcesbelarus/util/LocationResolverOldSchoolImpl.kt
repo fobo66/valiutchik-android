06835f62a27d662b63289aa29f91216c610c7552
Andrey Mukamolov <fobo66@protonmail.com>
2021-07-01 00:44:43 +0300

    Improved LocationManager provider

--- app/src/main/java/fobo66/exchangecourcesbelarus/util/LocationResolverOldSchoolImpl.kt
+++ app/src/main/java/fobo66/exchangecourcesbelarus/util/LocationResolverOldSchoolImpl.kt
@@ -4,20 +4,39 @@ import android.Manifest
 import android.app.Activity
 import android.location.Location
 import android.location.LocationManager
+import android.os.SystemClock
 import androidx.annotation.RequiresPermission
 import androidx.core.content.getSystemService
 import androidx.core.location.LocationManagerCompat
+import timber.log.Timber
+import java.time.Duration
 import javax.inject.Inject
 
 class LocationResolverOldSchoolImpl @Inject constructor() : LocationResolver {
+
+  private val locationFixTimeMaximum: Long by lazy {
+    Duration.ofHours(LOCATION_FIX_TIME_DURATION_HOURS).toNanos()
+  }
+
   @RequiresPermission(Manifest.permission.ACCESS_COARSE_LOCATION)
   override suspend fun resolveLocation(activity: Activity): Pair<Double, Double> {
     val noLocation = 0.0 to 0.0
     val locationManager = activity.getSystemService<LocationManager>() ?: return noLocation
 
     return if (LocationManagerCompat.isLocationEnabled(locationManager)) {
-      val location: Location? =
-        locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
+      var location: Location? = null
+      var tempLocation: Location?
+      var locationFixTime: Long
+
+      locationManager.getProviders(true).forEach {
+        Timber.v("Trying provider %s", it)
+        tempLocation = locationManager.getLastKnownLocation(it)
+        Timber.v("Location from provider %s : %s", it, tempLocation)
+        locationFixTime = tempLocation?.elapsedRealtimeNanos ?: 0
+        if (tempLocation != null && SystemClock.elapsedRealtimeNanos() - locationFixTime <= locationFixTimeMaximum) {
+          location = tempLocation
+        }
+      }
 
       location?.let {
         it.latitude to it.longitude
@@ -25,5 +44,9 @@ class LocationResolverOldSchoolImpl @Inject constructor() : LocationResolver {
     } else {
       noLocation
     }
+  }
+
+  companion object {
+    const val LOCATION_FIX_TIME_DURATION_HOURS = 3L
   }
 }
