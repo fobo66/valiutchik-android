a3868cf410eae4755916355634b46869c8b4fc33
Andrey Mukamolov <fobo66@protonmail.com>
2021-07-08 00:41:28 +0300

    Use required type in API

--- app/src/main/java/fobo66/exchangecourcesbelarus/api/ExchangeRatesApi.kt
+++ app/src/main/java/fobo66/exchangecourcesbelarus/api/ExchangeRatesApi.kt
@@ -1,11 +1,11 @@
 package fobo66.exchangecourcesbelarus.api
 
-import okhttp3.ResponseBody
+import fobo66.valiutchik.core.entities.Currency
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface ExchangeRatesApi {
 
   @GET("/outer/authXml/{city}")
-  suspend fun loadExchangeRates(@Path("city") cityIndex: String): ResponseBody
+  suspend fun loadExchangeRates(@Path("city") cityIndex: String): Set<Currency>
 }
