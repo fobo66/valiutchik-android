8efd55e0eaec11d395de4517b88b4e150557eac1
Andrey Mukamolov <fobo66@protonmail.com>
2021-07-08 00:30:14 +0300

    Added Retrofit response converter

---
+++ app/src/main/java/fobo66/exchangecourcesbelarus/util/XmlConverterFactory.kt
@@ -0,0 +1,21 @@
+package fobo66.exchangecourcesbelarus.util
+
+import fobo66.valiutchik.core.util.CurrencyRatesParser
+import okhttp3.ResponseBody
+import retrofit2.Converter
+import retrofit2.Retrofit
+import java.lang.reflect.Type
+import javax.inject.Inject
+
+class XmlConverterFactory @Inject constructor(
+  private val parser: CurrencyRatesParser
+) : Converter.Factory() {
+
+  override fun responseBodyConverter(
+    type: Type,
+    annotations: Array<out Annotation>,
+    retrofit: Retrofit
+  ): Converter<ResponseBody, *> {
+    return XmlConverter(parser)
+  }
+}
