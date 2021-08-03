300bf3457cbbce107b9af8ef248b4c72f68f7566
Andrey Mukamolov <amv@bamboogroup.eu>
2021-08-03 16:24:42 +0300

    Tweaked styles

--- app/src/main/java/fobo66/exchangecourcesbelarus/ui/ui/theme/Color.kt
+++ app/src/main/java/fobo66/exchangecourcesbelarus/ui/ui/theme/Color.kt
@@ -2,7 +2,7 @@ package fobo66.exchangecourcesbelarus.ui.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
+val Green500 = Color(0xff4caf50)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
