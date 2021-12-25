71e6ce002d1cb9471e2c6b394a913daf049b8dd5
Andrey Mukamolov <4471821+fobo66@users.noreply.github.com>
2021-12-26 02:07:52 +0300

    Formatting

--- app/src/main/java/fobo66/exchangecourcesbelarus/ui/NewMainActivity.kt
+++ app/src/main/java/fobo66/exchangecourcesbelarus/ui/NewMainActivity.kt
@@ -13,31 +13,31 @@ import androidx.compose.ui.tooling.preview.Preview
 import fobo66.exchangecourcesbelarus.ui.theme.ValiutchikTheme
 
 class NewMainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            ValiutchikTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContent {
+      ValiutchikTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+          modifier = Modifier.fillMaxSize(),
+          color = MaterialTheme.colors.background
+        ) {
+          Greeting("Android")
         }
+      }
     }
+  }
 }
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+  Text(text = "Hello $name!")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    ValiutchikTheme {
-        Greeting("Android")
-    }
+  ValiutchikTheme {
+    Greeting("Android")
+  }
 }
