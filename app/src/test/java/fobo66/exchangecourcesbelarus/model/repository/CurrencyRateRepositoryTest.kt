d6461b79e871c2778380648d5a6c548b43313e50
Andrey Mukamolov <4471821+fobo66@users.noreply.github.com>
2022-04-11 19:59:25 +0300

    Extracted anonymous fakes to classes

--- app/src/test/java/fobo66/exchangecourcesbelarus/model/repository/CurrencyRateRepositoryTest.kt
+++ app/src/test/java/fobo66/exchangecourcesbelarus/model/repository/CurrencyRateRepositoryTest.kt
@@ -1,26 +1,19 @@
 package fobo66.exchangecourcesbelarus.model.repository
 
-import fobo66.exchangecourcesbelarus.entities.BestCourse
-import fobo66.exchangecourcesbelarus.model.datasource.CurrencyRatesDataSource
-import fobo66.exchangecourcesbelarus.model.datasource.PersistenceDataSource
+import fobo66.exchangecourcesbelarus.model.fake.FakeBestCourseDataSource
+import fobo66.exchangecourcesbelarus.model.fake.FakeCurrencyRatesDataSource
+import fobo66.exchangecourcesbelarus.model.fake.FakePersistenceDataSource
 import fobo66.exchangecourcesbelarus.util.BankNameNormalizer
 import fobo66.exchangecourcesbelarus.util.CurrencyRatesLoadFailedException
-import fobo66.valiutchik.core.entities.Currency
-import fobo66.valiutchik.core.model.datasource.BestCourseDataSource
 import java.time.LocalDateTime
 import java.util.concurrent.Executors
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.asCoroutineDispatcher
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.emptyFlow
 import kotlinx.coroutines.test.runTest
-import okhttp3.ResponseBody.Companion.toResponseBody
 import org.junit.After
 import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
-import retrofit2.HttpException
-import retrofit2.Response
 
 /**
  * (c) 2019 Andrey Mukamolov <fobo66@protonmail.com>
@@ -31,30 +24,9 @@ class CurrencyRateRepositoryTest {
 
   private lateinit var currencyRateRepository: CurrencyRateRepository
 
-  private val bestCourseDataSource = object : BestCourseDataSource {
-    override fun findBestBuyCurrencies(courses: Set<Currency>): Map<String, Currency> = emptyMap()
-
-    override fun findBestSellCurrencies(courses: Set<Currency>): Map<String, Currency> = emptyMap()
-  }
-  private val persistenceDataSource = object : PersistenceDataSource {
-    var isSaved = false
-
-    override suspend fun saveBestCourses(bestCourses: List<BestCourse>) {
-      isSaved = true
-    }
-
-    override fun readBestCourses(): Flow<List<BestCourse>> = emptyFlow()
-  }
-
-  private val currencyRatesDataSource = object : CurrencyRatesDataSource {
-    var isError = false
-    override suspend fun loadExchangeRates(city: String): Set<Currency> =
-      if (isError) {
-        throw HttpException(Response.error<Set<Currency>>(500, "test".toResponseBody()))
-      } else {
-        setOf(Currency())
-      }
-  }
+  private val bestCourseDataSource = FakeBestCourseDataSource()
+  private val persistenceDataSource = FakePersistenceDataSource()
+  private val currencyRatesDataSource = FakeCurrencyRatesDataSource()
 
   private val ioDispatcher = Executors.newSingleThreadExecutor().asCoroutineDispatcher()
 
@@ -71,8 +43,8 @@ class CurrencyRateRepositoryTest {
 
   @After
   fun tearDown() {
-    persistenceDataSource.isSaved = false
-    currencyRatesDataSource.isError = false
+    persistenceDataSource.reset()
+    currencyRatesDataSource.reset()
     ioDispatcher.close()
   }
 
