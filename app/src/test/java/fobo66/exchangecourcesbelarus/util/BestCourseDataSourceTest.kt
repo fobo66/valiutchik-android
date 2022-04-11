2de671594113c64e04bbc8dc1745c0b8ba33b6c6
Andrey Mukamolov <4471821+fobo66@users.noreply.github.com>
2022-04-12 01:06:59 +0300

    Optimized test run a bit

--- app/src/test/java/fobo66/exchangecourcesbelarus/util/BestCourseDataSourceTest.kt
+++ app/src/test/java/fobo66/exchangecourcesbelarus/util/BestCourseDataSourceTest.kt
@@ -3,12 +3,10 @@ package fobo66.exchangecourcesbelarus.util
 import fobo66.valiutchik.core.EUR
 import fobo66.valiutchik.core.RUB
 import fobo66.valiutchik.core.USD
-import fobo66.valiutchik.core.entities.Currency
 import fobo66.valiutchik.core.model.datasource.BestCourseDataSource
 import fobo66.valiutchik.core.model.datasource.BestCourseDataSourceImpl
 import fobo66.valiutchik.core.util.CurrencyRatesParser
 import org.junit.Assert.assertEquals
-import org.junit.Before
 import org.junit.Test
 
 /**
@@ -16,24 +14,13 @@ import org.junit.Test
  * data from server saved in resources
  */
 class BestCourseDataSourceTest {
-  private lateinit var bestCourseDataSource: BestCourseDataSource
-  private lateinit var bestBuy: Map<String, Currency>
-  private lateinit var bestSell: Map<String, Currency>
-
   private val testFile = javaClass.classLoader?.getResourceAsStream("myfinTestData.xml")!!
+  private val parser: CurrencyRatesParser = TestMyfinParser()
+  private val currencies = parser.parse(testFile)
 
-  private val parser: CurrencyRatesParser by lazy {
-    TestMyfinParser()
-  }
-
-  @Before
-  fun setUp() {
-    bestCourseDataSource = BestCourseDataSourceImpl()
-    val currencies = parser.parse(testFile)
-
-    bestBuy = bestCourseDataSource.findBestBuyCurrencies(currencies)
-    bestSell = bestCourseDataSource.findBestSellCurrencies(currencies)
-  }
+  private val bestCourseDataSource: BestCourseDataSource = BestCourseDataSourceImpl()
+  private val bestBuy = bestCourseDataSource.findBestBuyCurrencies(currencies)
+  private val bestSell = bestCourseDataSource.findBestSellCurrencies(currencies)
 
   @Test
   fun testBestUSDBuyCoursesAreReallyBest() {
