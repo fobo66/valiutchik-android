25e7bbf6d54e8049767de6ed6637a317a9610886
Andrey Mukamolov <4471821+fobo66@users.noreply.github.com>
2022-04-28 23:57:38 +0300

    Updated Compose Material3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -171,7 +171,7 @@ dependencies {
 
   // compose
   implementation("androidx.compose.ui:ui:$composeVersion")
-  implementation("androidx.compose.material3:material3:1.0.0-alpha09")
+  implementation("androidx.compose.material3:material3:1.0.0-alpha10")
   implementation("androidx.compose.material:material:$composeVersion")
   implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
   implementation("androidx.activity:activity-compose:$activityVersion")
