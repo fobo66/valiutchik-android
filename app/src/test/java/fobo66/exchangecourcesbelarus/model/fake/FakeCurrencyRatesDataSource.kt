e3f060dc5c9639db0f47d896186d4aa2dc821ea5
Andrey Mukamolov <4471821+fobo66@users.noreply.github.com>
2022-05-09 13:25:30 +0300

    Fixed crash

--- app/src/test/java/fobo66/exchangecourcesbelarus/model/fake/FakeCurrencyRatesDataSource.kt
+++ app/src/test/java/fobo66/exchangecourcesbelarus/model/fake/FakeCurrencyRatesDataSource.kt
@@ -2,15 +2,13 @@ package fobo66.exchangecourcesbelarus.model.fake
 
 import fobo66.exchangecourcesbelarus.model.datasource.CurrencyRatesDataSource
 import fobo66.valiutchik.core.entities.Currency
-import okhttp3.ResponseBody.Companion.toResponseBody
-import retrofit2.HttpException
-import retrofit2.Response
+import java.io.IOException
 
 class FakeCurrencyRatesDataSource : CurrencyRatesDataSource, Resettable {
   var isError = false
   override suspend fun loadExchangeRates(city: String): Set<Currency> =
     if (isError) {
-      throw HttpException(Response.error<Set<Currency>>(500, "test".toResponseBody()))
+      throw IOException("test")
     } else {
       setOf(Currency())
     }
