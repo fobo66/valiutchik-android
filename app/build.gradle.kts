db5095540ddc7239f06b0ba60bc23e36dda781ed
Andrey Mukamolov <4471821+fobo66@users.noreply.github.com>
2022-05-16 11:37:27 +0300

    Updated LeakCanary to mitigate test crash

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -193,7 +193,8 @@ dependencies {
   implementation("dev.chrisbanes.insetter:insetter:0.6.1")
 
   // leakcanary
-  debugImplementation("com.squareup.leakcanary:leakcanary-android:2.9.1")
+  debugImplementation("com.squareup.leakcanary:leakcanary-android:2.10-SNAPSHOT")
+  debugImplementation("com.squareup.leakcanary:leakcanary-object-watcher-android-androidx:2.10-SNAPSHOT")
 
   coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:1.1.5")
 
