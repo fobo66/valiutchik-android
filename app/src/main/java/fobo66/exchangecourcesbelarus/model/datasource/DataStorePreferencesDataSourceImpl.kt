826007db01eb023583d55cd93763e528ba5085ab
Andrey Mukamolov <4471821+fobo66@users.noreply.github.com>
2022-06-24 20:03:35 +0300

    Improved synchronous API for preferences datasource

--- app/src/main/java/fobo66/exchangecourcesbelarus/model/datasource/DataStorePreferencesDataSourceImpl.kt
+++ app/src/main/java/fobo66/exchangecourcesbelarus/model/datasource/DataStorePreferencesDataSourceImpl.kt
@@ -7,14 +7,16 @@ import androidx.datastore.preferences.core.intPreferencesKey
 import androidx.datastore.preferences.core.stringPreferencesKey
 import javax.inject.Inject
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.last
+import kotlinx.coroutines.flow.lastOrNull
 import kotlinx.coroutines.flow.map
 
 class DataStorePreferencesDataSourceImpl @Inject constructor(
   private val dataStore: DataStore<Preferences>
 ) : PreferencesDataSource {
   override suspend fun loadString(key: String, defaultValue: String): String {
-    return dataStore.data.map { it[stringPreferencesKey(key)] }.last() ?: defaultValue
+    return dataStore.data.first()[stringPreferencesKey(key)] ?: defaultValue
   }
 
   fun observeString(key: String, defaultValue: String): Flow<String> {
@@ -28,7 +30,7 @@ class DataStorePreferencesDataSourceImpl @Inject constructor(
   }
 
   override suspend fun loadInt(key: String, defaultValue: Int): Int {
-    return dataStore.data.map { it[intPreferencesKey(key)] }.last() ?: defaultValue
+    return dataStore.data.first()[intPreferencesKey(key)] ?: defaultValue
   }
 
   fun observeInt(key: String, defaultValue: Int): Flow<Int> {
