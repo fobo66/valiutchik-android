0635e11ca2d4b97fca3eb8456161285f43c998ea
Andrey Mukamolov <4471821+fobo66@users.noreply.github.com>
2022-08-12 01:32:56 +0300

    Filled new activity with more actual logic

--- app/src/main/java/fobo66/exchangecourcesbelarus/ui/NewMainActivity.kt
+++ app/src/main/java/fobo66/exchangecourcesbelarus/ui/NewMainActivity.kt
@@ -3,41 +3,63 @@ package fobo66.exchangecourcesbelarus.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SmallTopAppBar
+import androidx.compose.material3.Text
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.stringResource
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import dagger.hilt.android.AndroidEntryPoint
+import fobo66.exchangecourcesbelarus.R.string
+import fobo66.exchangecourcesbelarus.ui.preferences.PreferenceScreen
+import fobo66.exchangecourcesbelarus.ui.preferences.PreferencesViewModel
 import fobo66.exchangecourcesbelarus.ui.theme.ValiutchikTheme
 
+@AndroidEntryPoint
 class NewMainActivity : ComponentActivity() {
+  @OptIn(ExperimentalMaterial3Api::class)
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContent {
+      val navController = rememberNavController()
+
       ValiutchikTheme {
-        // A surface container using the 'background' color from the theme
-        Surface(
-          modifier = Modifier.fillMaxSize(),
-          color = MaterialTheme.colors.background
-        ) {
-          Greeting("Android")
+        Scaffold(topBar = {
+          SmallTopAppBar(title = {
+            Text(text = stringResource(id = string.app_name))
+          })
+        }) {
+          NavHost(
+            navController = navController,
+            startDestination = "prefs",
+            modifier = Modifier.padding(it)
+          ) {
+            composable("prefs") {
+              val preferencesViewModel: PreferencesViewModel = hiltViewModel()
+
+              val defaultCity by preferencesViewModel.defaultCityPreference.collectAsState(
+                initial = ""
+              )
+
+              val updateInterval by preferencesViewModel.updateIntervalPreference.collectAsState(
+                initial = 1f
+              )
+
+              PreferenceScreen(
+                defaultCity,
+                updateInterval
+              )
+            }
+          }
         }
       }
     }
-  }
-}
-
-@Composable
-fun Greeting(name: String) {
-  Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-  ValiutchikTheme {
-    Greeting("Android")
   }
 }
