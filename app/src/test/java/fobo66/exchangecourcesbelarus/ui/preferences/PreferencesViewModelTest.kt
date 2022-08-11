922a4b503add39a5e9d96deb385423768b4f368a
Andrey Mukamolov <4471821+fobo66@users.noreply.github.com>
2022-08-12 00:07:40 +0300

    Added usecases for updating prefs to the viewmodel

--- app/src/test/java/fobo66/exchangecourcesbelarus/ui/preferences/PreferencesViewModelTest.kt
+++ app/src/test/java/fobo66/exchangecourcesbelarus/ui/preferences/PreferencesViewModelTest.kt
@@ -3,10 +3,12 @@ package fobo66.exchangecourcesbelarus.ui.preferences
 import app.cash.turbine.test
 import fobo66.valiutchik.core.usecases.LoadDefaultCityPreference
 import fobo66.valiutchik.core.usecases.LoadUpdateIntervalPreference
+import fobo66.valiutchik.core.usecases.UpdateDefaultCityPreference
+import fobo66.valiutchik.core.usecases.UpdateUpdateIntervalPreference
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runTest
@@ -20,18 +22,38 @@ import org.junit.jupiter.api.Test
 class PreferencesViewModelTest {
   private lateinit var viewModel: PreferencesViewModel
 
+  val defaultCity = MutableStateFlow("test")
+  val updateInterval = MutableStateFlow(1f)
+
   private val loadUpdateIntervalPreference = object : LoadUpdateIntervalPreference {
-    override fun execute(): Flow<Float> = flowOf(1f)
+    override fun execute(): Flow<Float> = updateInterval
   }
 
   private val loadDefaultCityPreference = object : LoadDefaultCityPreference {
-    override fun execute(): Flow<String> = flowOf("test")
+    override fun execute(): Flow<String> = defaultCity
+  }
+
+  private val updateUpdateIntervalPreference = object : UpdateUpdateIntervalPreference {
+    override suspend fun execute(newUpdateInterval: Float) {
+      updateInterval.emit(newUpdateInterval)
+    }
+  }
+
+  private val updateDefaultCityPreference = object : UpdateDefaultCityPreference {
+    override suspend fun execute(newDefaultCity: String) {
+      defaultCity.emit(newDefaultCity)
+    }
   }
 
   @BeforeEach
   fun setUp() {
     Dispatchers.setMain(UnconfinedTestDispatcher())
-    viewModel = PreferencesViewModel(loadDefaultCityPreference, loadUpdateIntervalPreference)
+    viewModel = PreferencesViewModel(
+      loadDefaultCityPreference,
+      loadUpdateIntervalPreference,
+      updateDefaultCityPreference,
+      updateUpdateIntervalPreference
+    )
   }
 
   @AfterEach
@@ -43,8 +65,17 @@ class PreferencesViewModelTest {
   fun `load default city pref`() = runTest {
     viewModel.defaultCityPreference.test {
       val awaitItem = awaitItem()
-      awaitComplete()
       assertEquals("test", awaitItem)
+    }
+  }
+
+  @Test
+  fun `update default city pref`() = runTest {
+    viewModel.updateDefaultCity("newcity")
+
+    viewModel.defaultCityPreference.test {
+      val awaitItem = awaitItem()
+      assertEquals("newcity", awaitItem)
     }
   }
 
@@ -52,8 +83,17 @@ class PreferencesViewModelTest {
   fun `load update interval pref`() = runTest {
     viewModel.updateIntervalPreference.test {
       val awaitItem = awaitItem()
-      awaitComplete()
       assertEquals(1f, awaitItem)
+    }
+  }
+
+  @Test
+  fun `update update interval pref`() = runTest {
+    viewModel.updateUpdateInterval(3f)
+
+    viewModel.updateIntervalPreference.test {
+      val awaitItem = awaitItem()
+      assertEquals(3f, awaitItem)
     }
   }
 }
