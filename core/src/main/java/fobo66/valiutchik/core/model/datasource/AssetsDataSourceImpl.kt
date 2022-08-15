c6494103176bc77b6c9d0a58b676dd74923b6bd2
Andrey Mukamolov <4471821+fobo66@users.noreply.github.com>
2022-08-15 20:16:06 +0300

    Fixed injection

--- core/src/main/java/fobo66/valiutchik/core/model/datasource/AssetsDataSourceImpl.kt
+++ core/src/main/java/fobo66/valiutchik/core/model/datasource/AssetsDataSourceImpl.kt
@@ -1,15 +1,15 @@
 package fobo66.valiutchik.core.model.datasource
 
-import android.content.Context
+import android.content.res.AssetManager
 import javax.inject.Inject
 import okio.BufferedSource
 import okio.buffer
 import okio.source
 
 class AssetsDataSourceImpl @Inject constructor(
-  private val context: Context
+  private val assetManager: AssetManager
 ) : AssetsDataSource {
   override fun loadFile(fileName: String): BufferedSource {
-    return context.assets.open(fileName).source().buffer()
+    return assetManager.open(fileName).source().buffer()
   }
 }
