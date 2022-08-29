a461f30098e9d3ab912e377e681c8047de75bedd
Andrey Mukamolov <4471821+fobo66@users.noreply.github.com>
2022-08-29 22:08:24 +0300

    Replaced last icon with composable

--- app/src/main/java/fobo66/exchangecourcesbelarus/ui/List.kt
+++ app/src/main/java/fobo66/exchangecourcesbelarus/ui/List.kt
@@ -10,12 +10,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import fobo66.exchangecourcesbelarus.R.drawable
 import fobo66.exchangecourcesbelarus.R.string
+import fobo66.exchangecourcesbelarus.ui.icons.NoRates
 import fobo66.exchangecourcesbelarus.ui.theme.ValiutchikTheme
 
 @Composable
@@ -30,7 +29,7 @@ fun NoRatesIndicator(modifier: Modifier = Modifier) {
   Box(modifier = modifier.fillMaxSize()) {
     Column(modifier = Modifier.align(Alignment.Center)) {
       Icon(
-        painter = painterResource(id = drawable.ic_no_rates),
+        NoRates,
         contentDescription = null,
         modifier = Modifier
           .padding(16.dp)
