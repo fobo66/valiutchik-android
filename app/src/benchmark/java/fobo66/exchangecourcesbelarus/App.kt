fe4431ba0ba0bd1d52c87ad3c2b8aa5a25bb0017
Andrey Mukamolov <4471821+fobo66@users.noreply.github.com>
2022-09-22 21:26:43 +0300

    Fixed benchmark run

---
+++ app/src/benchmark/java/fobo66/exchangecourcesbelarus/App.kt
@@ -0,0 +1,16 @@
+package fobo66.exchangecourcesbelarus
+
+import androidx.multidex.MultiDexApplication
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * (c) 2017 Andrey Mukamolov
+ * Created 9/16/17.
+ */
+@HiltAndroidApp
+open class App : MultiDexApplication() {
+
+  override fun onCreate() {
+    super.onCreate()
+  }
+}
