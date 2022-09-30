7acc2fa0408eeeb8a6e053b7e3139350fea2500b
Andrey Mukamolov <4471821+fobo66@users.noreply.github.com>
2022-10-01 00:33:19 +0300

    Updated deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 
 val composeVersion = "1.3.1"
 val composeUiVersion = "1.3.0-beta03"
-val accompanistVersion = "0.26.3-beta"
+val accompanistVersion = "0.26.4-beta"
 val kotlinCoroutinesVersion = "1.6.4"
 val hiltVersion = "2.44"
 val activityVersion = "1.6.0"
