643e149bfc1f34a8bf04ec9dc574d5ead3260a32
Andrey Mukamolov <4471821+fobo66@users.noreply.github.com>
2023-01-20 11:24:09 +0200

    Updated Compose compiler

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -114,7 +114,7 @@ dependencyResolutionManagement {
     }
 
     register("compose") {
-      version("compiler", "1.4.0-alpha02")
+      version("compiler", "1.4.0")
       version("compose", "1.4.0-alpha04")
       version("material", "1.1.0-alpha04")
       library("ui", "androidx.compose.ui", "ui").versionRef("compose")
