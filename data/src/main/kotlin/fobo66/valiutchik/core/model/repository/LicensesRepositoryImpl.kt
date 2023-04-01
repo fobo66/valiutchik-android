49025a86669c0ed4ffbb2b90249c4a637038d3c3
Andrey Mukamolov <4471821+fobo66@users.noreply.github.com>
2023-04-01 16:22:23 +0300

    Updated licenses
    Signed-off-by: Andrey Mukamolov <4471821+fobo66@users.noreply.github.com>

--- data/src/main/kotlin/fobo66/valiutchik/core/model/repository/LicensesRepositoryImpl.kt
+++ data/src/main/kotlin/fobo66/valiutchik/core/model/repository/LicensesRepositoryImpl.kt
@@ -1,5 +1,5 @@
 /*
- *    Copyright 2022 Andrey Mukamolov
+ *    Copyright 2023 Andrey Mukamolov
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -19,7 +19,6 @@ package fobo66.valiutchik.core.model.repository
 import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.adapter
-import fobo66.valiutchik.core.entities.OpenSourceLicenses
 import fobo66.valiutchik.core.entities.OpenSourceLicensesItem
 import fobo66.valiutchik.core.model.datasource.AssetsDataSource
 import javax.inject.Inject
@@ -30,12 +29,12 @@ class LicensesRepositoryImpl @Inject constructor(
 ) : LicensesRepository {
 
   @OptIn(ExperimentalStdlibApi::class)
-  private val jsonAdapter: JsonAdapter<OpenSourceLicenses> by lazy {
+  private val jsonAdapter: JsonAdapter<List<OpenSourceLicensesItem>> by lazy {
     moshi.adapter()
   }
 
   override fun loadLicenses(): List<OpenSourceLicensesItem> {
     val licensesFile = assetsDataSource.loadFile("open_source_licenses.json")
-    return jsonAdapter.fromJson(licensesFile)?.licenses ?: emptyList()
+    return jsonAdapter.fromJson(licensesFile) ?: emptyList()
   }
 }
