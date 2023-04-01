ca9d5688103d692098fcf06befa7f25923443f62
Andrey Mukamolov <4471821+fobo66@users.noreply.github.com>
2023-04-01 16:05:17 +0300

    Fixed deprecation
    Signed-off-by: Andrey Mukamolov <4471821+fobo66@users.noreply.github.com>

--- app/src/androidTest/kotlin/fobo66/exchangecourcesbelarus/ui/PreferencesScreenTest.kt
+++ app/src/androidTest/kotlin/fobo66/exchangecourcesbelarus/ui/PreferencesScreenTest.kt
@@ -1,5 +1,5 @@
 /*
- *    Copyright 2022 Andrey Mukamolov
+ *    Copyright 2023 Andrey Mukamolov
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -82,7 +82,7 @@ class PreferencesScreenTest : TestCase(
       onComposeScreen<SettingsScreen>(composeRule) {
         updateIntervalPreference.child<KNode> {
           hasTestTag("Slider")
-        }.performGesture {
+        }.performTouchInput {
           swipeRight()
         }
       }
