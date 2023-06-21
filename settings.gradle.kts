027eb3d681dfc69e7359ff69707ed5fb1e56771d
Andrey Mukamolov <4471821+fobo66@users.noreply.github.com>
2023-06-21 23:20:09 +0300

    Use stable AGP

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -59,7 +59,7 @@ dependencyResolutionManagement {
     }
 
     register("androidx") {
-      version("plugin", "8.2.0-alpha09")
+      version("plugin", "8.0.2")
       version("lifecycle", "2.6.1")
       version("espresso", "3.5.1")
       version("benchmark", "1.2.0-alpha15")
