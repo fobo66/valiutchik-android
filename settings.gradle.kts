6194e4ad7e3c0e8f02d43c3ec0428abb33361b7b
Andrey Mukamolov <4471821+fobo66@users.noreply.github.com>
2023-08-08 21:31:22 +0300

    Updated AGP

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -59,7 +59,7 @@ dependencyResolutionManagement {
     }
 
     register("androidx") {
-      version("plugin", "8.1.0")
+      version("plugin", "8.2.0-alpha15")
       version("lifecycle", "2.7.0-alpha01")
       version("espresso", "3.5.1")
       version("benchmark", "1.2.0-beta02")
