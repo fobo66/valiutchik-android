97c684669ea5efde67588c45f54067a6af2e22fc
Andrei Mukamolau <4471821+fobo66@users.noreply.github.com>
2024-01-26 21:19:00 +0200

    Updated non-dynamic theme colors

--- app/src/main/kotlin/fobo66/exchangecourcesbelarus/ui/theme/Theme.kt
+++ app/src/main/kotlin/fobo66/exchangecourcesbelarus/ui/theme/Theme.kt
@@ -1,5 +1,5 @@
 /*
- *    Copyright 2023 Andrey Mukamolov
+ *    Copyright 2024 Andrey Mukamolov
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -29,30 +29,73 @@ import androidx.glance.GlanceComposable
 import androidx.glance.GlanceTheme
 import androidx.glance.material3.ColorProviders
 
-private val DarkColorPalette = darkColorScheme(
-  primary = Green800,
-  primaryContainer = Green500,
-  secondary = Orange200
+private val LightColors = lightColorScheme(
+  primary = md_theme_light_primary,
+  onPrimary = md_theme_light_onPrimary,
+  primaryContainer = md_theme_light_primaryContainer,
+  onPrimaryContainer = md_theme_light_onPrimaryContainer,
+  secondary = md_theme_light_secondary,
+  onSecondary = md_theme_light_onSecondary,
+  secondaryContainer = md_theme_light_secondaryContainer,
+  onSecondaryContainer = md_theme_light_onSecondaryContainer,
+  tertiary = md_theme_light_tertiary,
+  onTertiary = md_theme_light_onTertiary,
+  tertiaryContainer = md_theme_light_tertiaryContainer,
+  onTertiaryContainer = md_theme_light_onTertiaryContainer,
+  error = md_theme_light_error,
+  errorContainer = md_theme_light_errorContainer,
+  onError = md_theme_light_onError,
+  onErrorContainer = md_theme_light_onErrorContainer,
+  background = md_theme_light_background,
+  onBackground = md_theme_light_onBackground,
+  surface = md_theme_light_surface,
+  onSurface = md_theme_light_onSurface,
+  surfaceVariant = md_theme_light_surfaceVariant,
+  onSurfaceVariant = md_theme_light_onSurfaceVariant,
+  outline = md_theme_light_outline,
+  inverseOnSurface = md_theme_light_inverseOnSurface,
+  inverseSurface = md_theme_light_inverseSurface,
+  inversePrimary = md_theme_light_inversePrimary,
+  surfaceTint = md_theme_light_surfaceTint,
+  outlineVariant = md_theme_light_outlineVariant,
+  scrim = md_theme_light_scrim,
 )
 
-private val LightColorPalette = lightColorScheme(
-  primary = Green500,
-  primaryContainer = Green200,
-  secondary = Orange500
-
-  /* Other default colors to override
-  background = Color.White,
-  surface = Color.White,
-  onPrimary = Color.White,
-  onSecondary = Color.Black,
-  onBackground = Color.Black,
-  onSurface = Color.Black,
-  */
+private val DarkColors = darkColorScheme(
+  primary = md_theme_dark_primary,
+  onPrimary = md_theme_dark_onPrimary,
+  primaryContainer = md_theme_dark_primaryContainer,
+  onPrimaryContainer = md_theme_dark_onPrimaryContainer,
+  secondary = md_theme_dark_secondary,
+  onSecondary = md_theme_dark_onSecondary,
+  secondaryContainer = md_theme_dark_secondaryContainer,
+  onSecondaryContainer = md_theme_dark_onSecondaryContainer,
+  tertiary = md_theme_dark_tertiary,
+  onTertiary = md_theme_dark_onTertiary,
+  tertiaryContainer = md_theme_dark_tertiaryContainer,
+  onTertiaryContainer = md_theme_dark_onTertiaryContainer,
+  error = md_theme_dark_error,
+  errorContainer = md_theme_dark_errorContainer,
+  onError = md_theme_dark_onError,
+  onErrorContainer = md_theme_dark_onErrorContainer,
+  background = md_theme_dark_background,
+  onBackground = md_theme_dark_onBackground,
+  surface = md_theme_dark_surface,
+  onSurface = md_theme_dark_onSurface,
+  surfaceVariant = md_theme_dark_surfaceVariant,
+  onSurfaceVariant = md_theme_dark_onSurfaceVariant,
+  outline = md_theme_dark_outline,
+  inverseOnSurface = md_theme_dark_inverseOnSurface,
+  inverseSurface = md_theme_dark_inverseSurface,
+  inversePrimary = md_theme_dark_inversePrimary,
+  surfaceTint = md_theme_dark_surfaceTint,
+  outlineVariant = md_theme_dark_outlineVariant,
+  scrim = md_theme_dark_scrim,
 )
 
 private val WidgetColors = ColorProviders(
-  light = LightColorPalette,
-  dark = DarkColorPalette
+  light = LightColors,
+  dark = DarkColors
 )
 
 @Composable
@@ -72,8 +115,8 @@ fun ValiutchikTheme(
       dynamicLightColorScheme(LocalContext.current)
     }
 
-    isDarkTheme -> DarkColorPalette
-    else -> LightColorPalette
+    isDarkTheme -> DarkColors
+    else -> LightColors
   }
 
   MaterialTheme(
