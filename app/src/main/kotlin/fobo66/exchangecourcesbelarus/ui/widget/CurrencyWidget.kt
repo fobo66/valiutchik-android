9cc93bf526c2975ec7185cf10fe154253337e532
Andrei Mukamolau <4471821+fobo66@users.noreply.github.com>
2024-02-11 21:10:15 +0200

    Disabled Glance preview for now

--- app/src/main/kotlin/fobo66/exchangecourcesbelarus/ui/widget/CurrencyWidget.kt
+++ app/src/main/kotlin/fobo66/exchangecourcesbelarus/ui/widget/CurrencyWidget.kt
@@ -1,5 +1,5 @@
 /*
- *    Copyright 2023 Andrey Mukamolov
+ *    Copyright 2024 Andrey Mukamolov
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -17,19 +17,13 @@
 package fobo66.exchangecourcesbelarus.ui.widget
 
 import android.content.Context
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
-import androidx.datastore.preferences.core.preferencesOf
 import androidx.glance.GlanceId
 import androidx.glance.GlanceModifier
 import androidx.glance.GlanceTheme
-import androidx.glance.appwidget.ExperimentalGlanceRemoteViewsApi
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.GlanceAppWidgetReceiver
 import androidx.glance.appwidget.appWidgetBackground
@@ -41,38 +35,48 @@ import androidx.glance.background
 import androidx.glance.layout.Column
 import androidx.glance.layout.padding
 import androidx.glance.text.Text
-import com.google.android.glance.appwidget.host.glance.GlanceAppWidgetHostPreview
 import dagger.hilt.android.AndroidEntryPoint
 import fobo66.exchangecourcesbelarus.ui.theme.ValiutchikWidgetTheme
-import fobo66.valiutchik.domain.R
 import fobo66.valiutchik.domain.entities.BestCurrencyRate
 import fobo66.valiutchik.domain.usecases.LoadExchangeRates
 import java.time.LocalDateTime
 import javax.inject.Inject
+import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.persistentListOf
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flowOf
+import kotlinx.collections.immutable.toImmutableList
+import kotlinx.coroutines.flow.map
 
 class CurrencyWidget(private val loadExchangeRates: LoadExchangeRates) : GlanceAppWidget() {
   override suspend fun provideGlance(context: Context, id: GlanceId) = provideContent {
-    val rates by loadExchangeRates.execute(LocalDateTime.now()).collectAsState(
-      initial = persistentListOf()
-    )
+    val rates: ImmutableList<BestCurrencyRate> by loadExchangeRates.execute(LocalDateTime.now())
+      .map { it.toImmutableList() }
+      .collectAsState(
+        initial = persistentListOf()
+      )
 
     ValiutchikWidgetTheme {
-      LazyColumn(
-        modifier = GlanceModifier
-          .background(GlanceTheme.colors.background)
-          .appWidgetBackground()
-          .cornerRadius(16.dp)
-      ) {
-        items(rates, { item -> item.id }) {
-          Column(modifier = GlanceModifier.padding(16.dp)) {
-            Text(text = context.getString(it.currencyNameRes))
-            Text(text = it.currencyValue)
-            Text(text = it.bank)
-          }
-        }
+      CurrencyWidgetContent(context = context, rates = rates)
+    }
+  }
+}
+
+@Composable
+fun CurrencyWidgetContent(
+  context: Context,
+  rates: ImmutableList<BestCurrencyRate>,
+  modifier: GlanceModifier = GlanceModifier
+) {
+  LazyColumn(
+    modifier = modifier
+      .background(GlanceTheme.colors.background)
+      .appWidgetBackground()
+      .cornerRadius(16.dp)
+  ) {
+    items(rates, { item -> item.id }) {
+      Column(modifier = GlanceModifier.padding(16.dp)) {
+        Text(text = context.getString(it.currencyNameRes))
+        Text(text = it.currencyValue)
+        Text(text = it.bank)
       }
     }
   }
@@ -86,36 +90,4 @@ class CurrencyAppWidgetReceiver : GlanceAppWidgetReceiver() {
 
   override val glanceAppWidget: GlanceAppWidget
     get() = CurrencyWidget(loadExchangeRates)
-}
-
-@OptIn(ExperimentalGlanceRemoteViewsApi::class)
-@Preview
-@Composable
-private fun MyAppWidgetPreview() {
-  val loadExchangeRates: LoadExchangeRates = object : LoadExchangeRates {
-    override fun execute(now: LocalDateTime): Flow<List<BestCurrencyRate>> =
-      flowOf(
-        persistentListOf(
-          BestCurrencyRate(
-            1,
-            "test",
-            R.string.currency_name_eur_buy,
-            "3.003"
-          )
-        )
-      )
-  }
-  // The size of the widget
-  val displaySize = DpSize(400.dp, 400.dp)
-  // Your GlanceAppWidget instance
-  val instance = CurrencyWidget(loadExchangeRates)
-  // Provide a state depending on the GlanceAppWidget state definition
-  val state = preferencesOf()
-
-  GlanceAppWidgetHostPreview(
-    modifier = Modifier.fillMaxSize(),
-    glanceAppWidget = instance,
-    state = state,
-    displaySize = displaySize,
-  )
 }
