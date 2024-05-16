8b3608a33b1336f83d2c18a68ab90916e8d20f88
Andrei Mukamolau <4471821+fobo66@users.noreply.github.com>
2024-05-16 21:45:52 +0300

    Replaced deprecated ClickableText with LinkAnnotation

--- app/src/main/kotlin/fobo66/exchangecourcesbelarus/ui/about/AboutAppDialog.kt
+++ app/src/main/kotlin/fobo66/exchangecourcesbelarus/ui/about/AboutAppDialog.kt
@@ -16,20 +16,15 @@
 
 package fobo66.exchangecourcesbelarus.ui.about
 
-import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.AlertDialogDefaults
-import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.ExperimentalTextApi
+import androidx.compose.ui.text.LinkAnnotation
 import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.UrlAnnotation
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
@@ -39,7 +34,6 @@ import fobo66.exchangecourcesbelarus.ui.theme.ValiutchikTheme
 private const val DESCRIPTION_URL = "https://myfin.by"
 private const val LINK_TEXT = "myfin.by"
 
-@OptIn(ExperimentalTextApi::class)
 @Composable
 fun AboutAppDialog(onDismiss: () -> Unit, modifier: Modifier = Modifier) {
   AlertDialog(
@@ -53,34 +47,20 @@ fun AboutAppDialog(onDismiss: () -> Unit, modifier: Modifier = Modifier) {
         val linkIndex = description.indexOf(LINK_TEXT)
         val linkEndIndex = linkIndex + LINK_TEXT.length
         append(description)
-        addStyle(
-          SpanStyle(
-            color = MaterialTheme.colorScheme.primary,
-            textDecoration = TextDecoration.Underline
-          ),
-          start = linkIndex,
-          end = linkEndIndex
-        )
-        addUrlAnnotation(
-          urlAnnotation = UrlAnnotation(DESCRIPTION_URL),
-          start = linkIndex,
-          end = linkEndIndex
+        addLink(
+          LinkAnnotation.Url(
+            url = DESCRIPTION_URL, style = SpanStyle(
+              color = MaterialTheme.colorScheme.primary,
+              textDecoration = TextDecoration.Underline
+            )
+          ), linkIndex, linkEndIndex
         )
       }
 
-      val uriHandler = LocalUriHandler.current
 
-      ClickableText(
-        text = descriptionAnnotatedText,
-        style = LocalTextStyle.current.copy(
-          color = AlertDialogDefaults.textContentColor
-        )
-      ) {
-        descriptionAnnotatedText.getUrlAnnotations(it, it)
-          .firstOrNull()?.let { range ->
-            uriHandler.openUri(range.item.url)
-          }
-      }
+      Text(
+        text = descriptionAnnotatedText
+      )
     },
     confirmButton = {
       TextButton(onClick = onDismiss) {
