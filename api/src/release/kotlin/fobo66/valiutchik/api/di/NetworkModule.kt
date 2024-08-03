f7af33b9eb28af7ae4926a098f7e678460a9899f
Andrei Mukamolau <4471821+fobo66@users.noreply.github.com>
2024-08-03 18:05:01 +0300

    Fixed release config for Ktor client

--- api/src/release/kotlin/fobo66/valiutchik/api/di/NetworkModule.kt
+++ api/src/release/kotlin/fobo66/valiutchik/api/di/NetworkModule.kt
@@ -20,6 +20,10 @@ import io.ktor.client.HttpClient
 import io.ktor.client.engine.okhttp.OkHttp
 import io.ktor.client.plugins.cache.HttpCache
 import io.ktor.client.plugins.cache.storage.FileStorage
+import io.ktor.client.plugins.compression.ContentEncoding
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
