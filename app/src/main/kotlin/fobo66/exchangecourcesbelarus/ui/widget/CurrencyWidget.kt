5b3c0aa8e0a9be20f4af83d80fdfb817ed49c53b
Andrei Mukamolau <4471821+fobo66@users.noreply.github.com>
2024-12-17 23:16:20 +0200

    Updated widget theming

--- app/src/main/kotlin/fobo66/exchangecourcesbelarus/ui/widget/CurrencyWidget.kt
+++ app/src/main/kotlin/fobo66/exchangecourcesbelarus/ui/widget/CurrencyWidget.kt
@@ -49,13 +49,19 @@ import kotlinx.datetime.Clock
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 
-class CurrencyWidget : GlanceAppWidget(), KoinComponent {
+class CurrencyWidget :
+  GlanceAppWidget(),
+  KoinComponent {
   private val loadExchangeRates: LoadExchangeRates by inject()
 
-  override suspend fun provideGlance(context: Context, id: GlanceId) = provideContent {
-    val rates: List<BestCurrencyRate> by loadExchangeRates.execute(Clock.System.now())
+  override suspend fun provideGlance(
+    context: Context,
+    id: GlanceId,
+  ) = provideContent {
+    val rates: List<BestCurrencyRate> by loadExchangeRates
+      .execute(Clock.System.now())
       .collectAsState(
-        initial = persistentListOf()
+        initial = persistentListOf(),
       )
 
     ValiutchikWidgetTheme {
@@ -68,36 +74,37 @@ class CurrencyWidget : GlanceAppWidget(), KoinComponent {
 fun CurrencyWidgetContent(
   context: Context,
   rates: List<BestCurrencyRate>,
-  modifier: GlanceModifier = GlanceModifier
+  modifier: GlanceModifier = GlanceModifier,
 ) {
   LazyColumn(
-    modifier = modifier
-      .background(GlanceTheme.colors.background)
-      .appWidgetBackground()
-      .cornerRadius(16.dp)
+    modifier =
+      modifier
+        .background(GlanceTheme.colors.widgetBackground)
+        .appWidgetBackground()
+        .cornerRadius(16.dp),
   ) {
     items(rates, { item -> item.id }) {
       Column(modifier = GlanceModifier.padding(16.dp)) {
         Text(
           text = context.getString(it.currencyNameRes),
-          style = TextStyle(color = GlanceTheme.colors.onBackground)
+          style = TextStyle(color = GlanceTheme.colors.primary),
         )
         Text(
           text = it.currencyValue,
-          style = TextStyle(
-            color = GlanceTheme.colors.onBackground,
-            fontSize = 24.sp,
-            fontWeight = FontWeight.Bold
-          )
+          style =
+            TextStyle(
+              color = GlanceTheme.colors.primary,
+              fontSize = 24.sp,
+              fontWeight = FontWeight.Bold,
+            ),
         )
-        Text(text = it.bank, style = TextStyle(color = GlanceTheme.colors.onBackground))
+        Text(text = it.bank, style = TextStyle(color = GlanceTheme.colors.primary))
       }
     }
   }
 }
 
 class CurrencyAppWidgetReceiver : GlanceAppWidgetReceiver() {
-
   override val glanceAppWidget: GlanceAppWidget
     get() = CurrencyWidget()
 }
@@ -107,8 +114,9 @@ class CurrencyAppWidgetReceiver : GlanceAppWidgetReceiver() {
 @Composable
 private fun CurrencyWidgetPreview() {
   CurrencyWidgetContent(
-    LocalContext.current, listOf(
-      BestCurrencyRate(0, "test", fobo66.valiutchik.domain.R.string.currency_name_eur_buy, "1.23")
-    )
+    LocalContext.current,
+    listOf(
+      BestCurrencyRate(0, "test", fobo66.valiutchik.domain.R.string.currency_name_eur_buy, "1.23"),
+    ),
   )
 }
