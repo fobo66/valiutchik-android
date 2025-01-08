988f38dcc51b7682dc3e035e84b0466921f498ef
Andrei Mukamolau <4471821+fobo66@users.noreply.github.com>
2025-01-08 21:04:46 +0200

    Added deps for xml

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,5 +1,5 @@
 /*
- *    Copyright 2024 Andrey Mukamolov
+ *    Copyright 2025 Andrey Mukamolov
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -17,89 +17,90 @@
 import com.android.sdklib.AndroidVersion
 
 plugins {
-  alias(libs.plugins.android.library)
-  kotlin("android")
-  kotlin("plugin.serialization")
-  alias(libs.plugins.detekt)
-  alias(libs.plugins.junit)
+    alias(libs.plugins.android.library)
+    kotlin("android")
+    kotlin("plugin.serialization")
+    alias(libs.plugins.detekt)
+    alias(libs.plugins.junit)
 }
 
 android {
-  namespace = "fobo66.valiutchik.api"
-  compileSdk = AndroidVersion.VersionCodes.VANILLA_ICE_CREAM
+    namespace = "fobo66.valiutchik.api"
+    compileSdk = AndroidVersion.VersionCodes.VANILLA_ICE_CREAM
 
-  defaultConfig {
-    minSdk = AndroidVersion.VersionCodes.O
+    defaultConfig {
+        minSdk = AndroidVersion.VersionCodes.O
 
-    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    consumerProguardFiles("consumer-rules.pro")
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
 
-    resValue(
-      "string",
-      "apiUsername",
-      loadSecret(rootProject, API_USERNAME)
-    )
+        resValue(
+            "string",
+            "apiUsername",
+            loadSecret(rootProject, API_USERNAME)
+        )
 
-    resValue(
-      "string",
-      "apiPassword",
-      loadSecret(rootProject, API_PASSWORD)
-    )
+        resValue(
+            "string",
+            "apiPassword",
+            loadSecret(rootProject, API_PASSWORD)
+        )
 
-    resValue(
-      "string",
-      "geocoderApiKey",
-      loadSecret(rootProject, GEOCODER_TOKEN)
-    )
-  }
-
-  buildTypes {
-    release {
-      proguardFiles(
-        getDefaultProguardFile("proguard-android-optimize.txt"),
-        "proguard-rules.pro"
-      )
+        resValue(
+            "string",
+            "geocoderApiKey",
+            loadSecret(rootProject, GEOCODER_TOKEN)
+        )
     }
-  }
-  compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
-  }
-  kotlinOptions {
-    jvmTarget = "17"
-  }
+
+    buildTypes {
+        release {
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
 
 detekt {
-  autoCorrect = true
+    autoCorrect = true
 }
 
 dependencies {
-  implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.core)
 
-  implementation(platform(libs.koin.bom))
-  implementation(libs.koin.android)
+    implementation(platform(libs.koin.bom))
+    implementation(libs.koin.android)
 
-  implementation(platform(libs.okhttp.bom))
-  implementation(libs.okhttp.core)
-  debugImplementation(libs.ktor.logging)
-  implementation(libs.ktor.client)
-  implementation(libs.ktor.auth)
-  implementation(libs.ktor.content)
-  implementation(libs.ktor.encoding)
-  implementation(libs.ktor.serialization)
-  implementation(libs.kotlinx.serialization)
+    implementation(platform(libs.okhttp.bom))
+    implementation(libs.okhttp.core)
+    debugImplementation(libs.ktor.logging)
+    implementation(libs.ktor.client)
+    implementation(libs.ktor.auth)
+    implementation(libs.ktor.content)
+    implementation(libs.ktor.encoding)
+    implementation(libs.ktor.serialization)
+    implementation(libs.ktor.serialization.xml)
+    implementation(libs.kotlinx.serialization)
 
-  implementation(libs.napier)
+    implementation(libs.napier)
 
-  detektPlugins(libs.detekt.rules.formatting)
-  detektPlugins(libs.detekt.rules.compose)
+    detektPlugins(libs.detekt.rules.formatting)
+    detektPlugins(libs.detekt.rules.compose)
 
-  testImplementation(libs.junit.api)
-  testImplementation(platform(libs.koin.bom))
-  testImplementation(libs.koin.test)
-  testRuntimeOnly(libs.junit.engine)
+    testImplementation(libs.junit.api)
+    testImplementation(platform(libs.koin.bom))
+    testImplementation(libs.koin.test)
+    testRuntimeOnly(libs.junit.engine)
 
-  androidTestImplementation(libs.androidx.test.rules)
-  androidTestImplementation(libs.androidx.test.junit)
+    androidTestImplementation(libs.androidx.test.rules)
+    androidTestImplementation(libs.androidx.test.junit)
 }
