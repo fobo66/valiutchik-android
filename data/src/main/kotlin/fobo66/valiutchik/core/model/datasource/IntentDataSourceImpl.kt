95040963b278754f373b42d09b51e07d46541ed8
Andrei Mukamolau <4471821+fobo66@users.noreply.github.com>
2025-04-25 20:46:08 +0300

    Use multiplatform URI library in IntentDataSource

--- data/src/main/kotlin/fobo66/valiutchik/core/model/datasource/IntentDataSourceImpl.kt
+++ data/src/main/kotlin/fobo66/valiutchik/core/model/datasource/IntentDataSourceImpl.kt
@@ -1,5 +1,5 @@
 /*
- *    Copyright 2024 Andrey Mukamolov
+ *    Copyright 2025 Andrey Mukamolov
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -19,12 +19,13 @@ package fobo66.valiutchik.core.model.datasource
 import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
-import android.net.Uri
+import com.eygraber.uri.Uri
+import com.eygraber.uri.toAndroidUri
 
 class IntentDataSourceImpl(
   private val context: Context
 ) : IntentDataSource {
-  override fun createIntent(uri: Uri, action: String): Intent = Intent(action, uri)
+  override fun createIntent(uri: Uri, action: String): Intent = Intent(action, uri.toAndroidUri())
 
   override fun resolveIntent(intent: Intent): ComponentName? =
     intent.resolveActivity(context.packageManager)
