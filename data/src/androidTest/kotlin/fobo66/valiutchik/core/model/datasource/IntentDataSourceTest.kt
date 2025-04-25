819447ddbcffcccf119732c1cdeb1224d2c341de
Andrei Mukamolau <4471821+fobo66@users.noreply.github.com>
2025-04-25 21:48:00 +0300

    Fixed test

--- data/src/androidTest/kotlin/fobo66/valiutchik/core/model/datasource/IntentDataSourceTest.kt
+++ data/src/androidTest/kotlin/fobo66/valiutchik/core/model/datasource/IntentDataSourceTest.kt
@@ -17,10 +17,10 @@
 package fobo66.valiutchik.core.model.datasource
 
 import android.content.Intent
-import android.net.Uri
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.intent.rule.IntentsRule
 import androidx.test.filters.SmallTest
+import com.eygraber.uri.Uri
 import com.google.common.truth.Truth.assertThat
 import org.junit.Rule
 import org.junit.Test
