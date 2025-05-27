55e4eba91458706b603373ef6dd7dff47fbf039c
Andrei Mukamolau <4471821+fobo66@users.noreply.github.com>
2025-05-27 18:44:35 +0300

    Drafted query for loading best rate

--- data/src/main/kotlin/fobo66/valiutchik/core/db/RatesDao.kt
+++ data/src/main/kotlin/fobo66/valiutchik/core/db/RatesDao.kt
@@ -17,11 +17,33 @@
 package fobo66.valiutchik.core.db
 
 import androidx.room.Dao
+import androidx.room.Query
 import androidx.room.Upsert
+import fobo66.valiutchik.core.entities.BestRate
 import fobo66.valiutchik.core.entities.Rate
 
 @Dao
 interface RatesDao {
     @Upsert
     suspend fun insertRates(rates: List<Rate>)
+
+    @Query("SELECT max(eurBuy) AS bestRate, bankName FROM rates ORDER by date(date) DESC")
+    suspend fun resolveBestEurBuyRate(): BestRate
+
+    @Query(
+        "SELECT CASE WHEN :rate = 'usdBuy' THEN max(usdBuy) " +
+            "WHEN :rate = 'usdSell' THEN min(usdSell) " +
+            "WHEN :rate = 'eurBuy' THEN max(eurBuy) " +
+            "WHEN :rate = 'eurSell' THEN min(eurSell) " +
+            "WHEN :rate = 'rubBuy' THEN max(rubBuy) " +
+            "WHEN :rate = 'rubSell' THEN min(rubSell) " +
+            "WHEN :rate = 'plnBuy' THEN max(plnBuy) " +
+            "WHEN :rate = 'plnSell' THEN min(plnSell) " +
+            "WHEN :rate = 'uahBuy' THEN max(uahBuy) " +
+            "WHEN :rate = 'uahSell' THEN min(uahSell) " +
+            "END AS bestRate, bankName FROM rates " +
+            "WHERE bestRate != 0.0 " +
+            "ORDER by date(date) DESC"
+    )
+    suspend fun resolveBestRate(rate: String): BestRate
 }
