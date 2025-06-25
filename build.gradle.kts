5f3ae756ddf4b08b7c280840bb3f0a3d66cc50a7
Andrei Mukamolau <4471821+fobo66@users.noreply.github.com>
2025-06-25 22:42:20 +0300

    Added multiplatform plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,7 @@ plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.android.test) apply false
     alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.compose) apply false
     alias(libs.plugins.licenses) apply false
