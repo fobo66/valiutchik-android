4f04caf2c2df2ac89b4377d4097f72a9b7261a9c
Andrei Mukamolau <4471821+fobo66@users.noreply.github.com>
2025-07-05 09:11:59 +0300

    Fixed test

--- data/src/androidHostTest/kotlin/fobo66/valiutchik/core/model/repository/LocationRepositoryTest.kt
+++ data/src/androidHostTest/kotlin/fobo66/valiutchik/core/model/repository/LocationRepositoryTest.kt
@@ -55,7 +55,7 @@ class LocationRepositoryTest {
         geocodingDataSource.unexpectedError = true
 
         runTest {
-            assertThrows<KotlinNullPointerException> {
+            assertThrows<NullPointerException> {
                 locationRepository.resolveUserCity(DEFAULT)
             }
         }
