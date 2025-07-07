ec4de713ca1e02e8f574ac3a046e1dc0532ba759
Andrei Mukamolau <4471821+fobo66@users.noreply.github.com>
2025-07-07 20:50:58 +0300

    Moved strings to the app

--- domain/src/main/kotlin/fobo66/valiutchik/domain/usecases/LoadExchangeRatesImpl.kt
+++ domain/src/main/kotlin/fobo66/valiutchik/domain/usecases/LoadExchangeRatesImpl.kt
@@ -16,44 +16,21 @@
 
 package fobo66.valiutchik.domain.usecases
 
-import androidx.collection.ScatterMap
-import androidx.collection.mutableScatterMapOf
 import fobo66.valiutchik.core.entities.BestCourse
 import fobo66.valiutchik.core.model.repository.CurrencyRateRepository
 import fobo66.valiutchik.core.util.CurrencyName
-import fobo66.valiutchik.domain.R
 import fobo66.valiutchik.domain.entities.NewBestCurrencyRate
 import fobo66.valiutchik.domain.entities.NewBestCurrencyRate.DollarBuyRate
 import fobo66.valiutchik.domain.entities.NewBestCurrencyRate.EuroBuyRate
 import fobo66.valiutchik.domain.entities.NewBestCurrencyRate.HryvniaBuyRate
 import fobo66.valiutchik.domain.entities.NewBestCurrencyRate.RubleBuyRate
 import fobo66.valiutchik.domain.entities.NewBestCurrencyRate.ZlotyBuyRate
-import kotlin.LazyThreadSafetyMode.NONE
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
 class LoadExchangeRatesImpl(private val currencyRateRepository: CurrencyRateRepository) :
     LoadExchangeRates {
-    private val buyLabels: ScatterMap<CurrencyName, Int> by lazy(NONE) {
-        mutableScatterMapOf(
-            CurrencyName.DOLLAR to R.string.currency_name_usd_buy,
-            CurrencyName.EUR to R.string.currency_name_eur_buy,
-            CurrencyName.RUB to R.string.currency_name_rub_buy,
-            CurrencyName.PLN to R.string.currency_name_pln_buy,
-            CurrencyName.UAH to R.string.currency_name_uah_buy
-        )
-    }
-
-    private val sellLabels: ScatterMap<CurrencyName, Int> by lazy(NONE) {
-        mutableScatterMapOf(
-            CurrencyName.DOLLAR to R.string.currency_name_usd_sell,
-            CurrencyName.EUR to R.string.currency_name_eur_sell,
-            CurrencyName.RUB to R.string.currency_name_rub_sell,
-            CurrencyName.PLN to R.string.currency_name_pln_sell,
-            CurrencyName.UAH to R.string.currency_name_uah_sell
-        )
-    }
 
     @OptIn(ExperimentalCoroutinesApi::class)
     override fun execute(): Flow<List<NewBestCurrencyRate>> =
