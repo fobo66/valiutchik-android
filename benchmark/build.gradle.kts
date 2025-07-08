de0c91ed25bf4a9e22f549936e39a54cf9696b27
Andrei Mukamolau <4471821+fobo66@users.noreply.github.com>
2025-07-08 19:37:41 +0300

    Use test bank builder method in benchmark

--- benchmark/build.gradle.kts
+++ benchmark/build.gradle.kts
@@ -70,6 +70,7 @@ dependencies {
     androidTestImplementation(libs.room.testing)
     androidTestImplementation(libs.kotlinx.coroutines.test)
     androidTestImplementation(libs.kotlinx.datetime)
+    androidTestImplementation(project(":data-testing"))
     detektPlugins(libs.detekt.rules.formatting)
     detektPlugins(libs.detekt.rules.compose)
 }
--- benchmark/src/androidTest/kotlin/fobo66/valiutchik/benchmark/RateMapperBenchmark.kt
+++ benchmark/src/androidTest/kotlin/fobo66/valiutchik/benchmark/RateMapperBenchmark.kt
@@ -19,55 +19,27 @@ package fobo66.valiutchik.benchmark
 import androidx.benchmark.junit4.BenchmarkRule
 import androidx.benchmark.junit4.measureRepeated
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import dev.fobo66.core.data.testing.fake.buildBank
 import fobo66.valiutchik.api.entity.Bank
-import fobo66.valiutchik.api.entity.UNDEFINED_RATE
+import fobo66.valiutchik.api.entity.UNDEFINED_BUY_RATE
+import fobo66.valiutchik.api.entity.UNDEFINED_SELL_RATE
 import fobo66.valiutchik.core.entities.toRate
 import kotlinx.datetime.LocalDate
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-private const val ID = 1L
-private const val NAME = "test"
-private const val RATE = 1.23
-private const val DATE = "2025-06-16"
-
 @RunWith(AndroidJUnit4::class)
 class RateMapperBenchmark {
     @get:Rule
     val benchmarkRule = BenchmarkRule()
 
-    private val fullBank = Bank(
-        bankId = ID,
-        filialId = ID,
-        date = DATE,
-        bankName = NAME,
-        usdBuy = RATE,
-        usdSell = RATE,
-        eurBuy = RATE,
-        eurSell = RATE,
-        rubBuy = RATE,
-        rubSell = RATE,
-        plnBuy = RATE,
-        plnSell = RATE,
-        uahBuy = RATE,
-        uahSell = RATE
-    )
-    private val partialBank = Bank(
-        bankId = ID,
-        filialId = ID,
-        date = DATE,
-        bankName = NAME,
-        usdBuy = RATE,
-        usdSell = RATE,
-        eurBuy = RATE,
-        eurSell = RATE,
-        rubBuy = UNDEFINED_RATE,
-        rubSell = UNDEFINED_RATE,
-        plnBuy = RATE,
-        plnSell = UNDEFINED_RATE,
-        uahBuy = RATE,
-        uahSell = UNDEFINED_RATE
+    private val fullBank = buildBank()
+    private val partialBank = buildBank(
+        rubBuy = UNDEFINED_BUY_RATE,
+        rubSell = UNDEFINED_SELL_RATE,
+        plnSell = UNDEFINED_SELL_RATE,
+        uahSell = UNDEFINED_SELL_RATE
     )
 
     private val emptyBank = Bank()
