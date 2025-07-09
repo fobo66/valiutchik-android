7327201cdba7093e016985291e0a27ff2ccae712
Andrei Mukamolau <4471821+fobo66@users.noreply.github.com>
2025-07-09 21:05:46 +0300

    Changed intent datasource API

--- data/src/jvmMain/kotlin/fobo66/valiutchik/core/model/datasource/IntentDataSourceDesktopImpl.kt
+++ data/src/jvmMain/kotlin/fobo66/valiutchik/core/model/datasource/IntentDataSourceDesktopImpl.kt
@@ -17,12 +17,35 @@
 package fobo66.valiutchik.core.model.datasource
 
 import com.eygraber.uri.Uri
+import com.eygraber.uri.toURI
+import io.github.aakira.napier.Napier
 import java.awt.Desktop
+import java.awt.HeadlessException
+import kotlinx.io.IOException
 
 class IntentDataSourceDesktopImpl : IntentDataSource {
-    override fun createIntentUri(uri: Uri, action: String): String = uri.toString()
+    override fun resolveIntent(intentUri: Uri): Boolean = try {
+        val desktop = Desktop.getDesktop()
+        desktop.browse(intentUri.toURI())
+        true
+    } catch (e: UnsupportedOperationException) {
+        Napier.e(e) {
+            "Opening browser is not supported"
+        }
+        false
+    } catch (e: IOException) {
+        Napier.e(e) {
+            "Failed to open browser"
+        }
+        false
+    } catch (e: HeadlessException) {
+        Napier.e(e) {
+            "Cannot open browser in headless mode"
+        }
+        false
+    }
 
-    override fun checkIntentUri(uri: String): Boolean = Desktop.isDesktopSupported() &&
+    override fun checkIntentUri(uri: Uri): Boolean = Desktop.isDesktopSupported() &&
         Desktop.getDesktop()
             .isSupported(Desktop.Action.BROWSE)
 }
