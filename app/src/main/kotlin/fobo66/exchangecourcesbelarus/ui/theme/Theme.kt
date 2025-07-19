b17f47301a11addd7ca93f4890d857a60a4f48f5
Andrei Mukamolau <4471821+fobo66@users.noreply.github.com>
2025-07-19 18:54:47 +0300

    Cleaned up unused theme elements

--- app/src/main/kotlin/fobo66/exchangecourcesbelarus/ui/theme/Theme.kt
+++ app/src/main/kotlin/fobo66/exchangecourcesbelarus/ui/theme/Theme.kt
@@ -20,45 +20,12 @@ import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.expressiveLightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.glance.GlanceComposable
 import androidx.glance.GlanceTheme
 import androidx.glance.material3.ColorProviders
 import dev.fobo66.valiutchik.ui.theme.AppTheme
-
-private val LightColors =
-    lightColorScheme(
-        primary = md_theme_light_primary,
-        onPrimary = md_theme_light_onPrimary,
-        primaryContainer = md_theme_light_primaryContainer,
-        onPrimaryContainer = md_theme_light_onPrimaryContainer,
-        secondary = md_theme_light_secondary,
-        onSecondary = md_theme_light_onSecondary,
-        secondaryContainer = md_theme_light_secondaryContainer,
-        onSecondaryContainer = md_theme_light_onSecondaryContainer,
-        tertiary = md_theme_light_tertiary,
-        onTertiary = md_theme_light_onTertiary,
-        tertiaryContainer = md_theme_light_tertiaryContainer,
-        onTertiaryContainer = md_theme_light_onTertiaryContainer,
-        error = md_theme_light_error,
-        errorContainer = md_theme_light_errorContainer,
-        onError = md_theme_light_onError,
-        onErrorContainer = md_theme_light_onErrorContainer,
-        background = md_theme_light_background,
-        onBackground = md_theme_light_onBackground,
-        surface = md_theme_light_surface,
-        onSurface = md_theme_light_onSurface,
-        surfaceVariant = md_theme_light_surfaceVariant,
-        onSurfaceVariant = md_theme_light_onSurfaceVariant,
-        outline = md_theme_light_outline,
-        inverseOnSurface = md_theme_light_inverseOnSurface,
-        inverseSurface = md_theme_light_inverseSurface,
-        inversePrimary = md_theme_light_inversePrimary,
-        surfaceTint = md_theme_light_surfaceTint,
-        outlineVariant = md_theme_light_outlineVariant,
-        scrim = md_theme_light_scrim
-    )
 
 private val DarkColors =
     darkColorScheme(
@@ -93,9 +60,10 @@ private val DarkColors =
         scrim = md_theme_dark_scrim
     )
 
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
 private val WidgetColors =
     ColorProviders(
-        light = LightColors,
+        light = expressiveLightColorScheme(),
         dark = DarkColors
     )
 
