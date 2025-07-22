5b4b4c313b3240ff75fc12f533a17a94e0d95d9b
Andrei Mukamolau <4471821+fobo66@users.noreply.github.com>
2025-07-22 18:01:08 +0300

    Add icons

--- ui/build.gradle.kts
+++ ui/build.gradle.kts
@@ -72,6 +72,7 @@ kotlin {
                 implementation(compose.foundation)
                 implementation(compose.ui)
                 implementation(compose.material3)
+                implementation(compose.materialIconsExtended)
                 implementation(compose.material3AdaptiveNavigationSuite)
                 implementation(compose.components.resources)
                 implementation(compose.components.uiToolingPreview)
