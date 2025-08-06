f9acc7c230c215d9a3bda97e8f71dec4fc09a333
Andrei Mukamolau <4471821+fobo66@users.noreply.github.com>
2025-08-06 23:06:20 +0300

    Changed main window icon

--- desktop/src/desktopMain/kotlin/dev/fobo66/valiutchik/desktop/rates/RatesWindow.kt
+++ desktop/src/desktopMain/kotlin/dev/fobo66/valiutchik/desktop/rates/RatesWindow.kt
@@ -18,7 +18,6 @@ package dev.fobo66.valiutchik.desktop.rates
 
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Snackbar
 import androidx.compose.material3.SnackbarHost
@@ -26,21 +25,21 @@ import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.window.Window
 import dev.fobo66.valiutchik.ui.TAG_SNACKBAR
-import dev.fobo66.valiutchik.ui.icon.Bank
 import dev.fobo66.valiutchik.ui.rates.RatesPanel
 import dev.fobo66.valiutchik.ui.theme.AppTheme
+import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 import valiutchik.desktop.generated.resources.Res
 import valiutchik.desktop.generated.resources.app_name
+import valiutchik.desktop.generated.resources.ic_launcher
 
 @Composable
 fun RatesWindow(onClose: () -> Unit, onOpenSettings: () -> Unit) {
     Window(
-        icon = rememberVectorPainter(Icons.Default.Bank),
+        icon = painterResource(Res.drawable.ic_launcher),
         onCloseRequest = onClose,
         title = stringResource(Res.string.app_name)
     ) {
