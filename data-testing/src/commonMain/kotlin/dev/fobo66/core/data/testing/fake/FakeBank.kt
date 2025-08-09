bf167e369e0de3a6382de160ce81adb34ebf5c5c
Andrei Mukamolau <4471821+fobo66@users.noreply.github.com>
2025-08-09 19:59:38 +0300

    Refactor mapper benchmark for new mapper

--- data-testing/src/commonMain/kotlin/dev/fobo66/core/data/testing/fake/FakeBank.kt
+++ data-testing/src/commonMain/kotlin/dev/fobo66/core/data/testing/fake/FakeBank.kt
@@ -16,19 +16,25 @@
 
 package dev.fobo66.core.data.testing.fake
 
-import fobo66.valiutchik.api.entity.Bank
+import fobo66.valiutchik.api.CURRENCY_ALIAS_EURO
+import fobo66.valiutchik.api.CURRENCY_ALIAS_HRYVNIA
+import fobo66.valiutchik.api.CURRENCY_ALIAS_RUBLE
+import fobo66.valiutchik.api.CURRENCY_ALIAS_US_DOLLAR
+import fobo66.valiutchik.api.CURRENCY_ALIAS_ZLOTY
+import fobo66.valiutchik.api.entity.Currency
+import fobo66.valiutchik.api.entity.CurrencyRateSource
 
 const val ID = 1L
 const val TEST = "test"
 const val RATE = 1.23f
-const val DATE = "2025-07-01"
-const val RAW_DATE = "01.07.2025"
+const val DATE = 1754725825L
+const val PROCESSED_DATE = "2025-08-09T07:50:25Z"
 
 @Suppress("LongParameterList") // ok for tests
 fun buildBank(
     bankId: Long = ID,
-    filialId: Long = ID,
-    date: String = DATE,
+    branchId: Long = ID,
+    date: Long = DATE,
     bankName: String = TEST,
     usdBuy: Float = RATE,
     usdSell: Float = RATE,
@@ -40,19 +46,60 @@ fun buildBank(
     plnSell: Float = RATE,
     uahBuy: Float = RATE,
     uahSell: Float = RATE
-): Bank = Bank(
-    bankId,
-    filialId,
-    date,
-    bankName,
-    usdBuy,
-    usdSell,
-    eurBuy,
-    eurSell,
-    rubBuy,
-    rubSell,
-    plnBuy,
-    plnSell,
-    uahBuy,
-    uahSell
+): List<CurrencyRateSource> = listOf(
+    CurrencyRateSource(
+        id = branchId,
+        bankId = bankId,
+        bankName = bankName,
+        currency = Currency(
+            buy = usdBuy,
+            dateUpdate = date,
+            name = CURRENCY_ALIAS_US_DOLLAR,
+            sell = usdSell
+        )
+    ),
+    CurrencyRateSource(
+        bankId = bankId,
+        bankName = bankName,
+        currency = Currency(
+            buy = eurBuy,
+            dateUpdate = date,
+            name = CURRENCY_ALIAS_EURO,
+            sell = eurSell
+        ),
+        id = branchId
+    ),
+    CurrencyRateSource(
+        id = branchId,
+        bankId = bankId,
+        bankName = bankName,
+        currency = Currency(
+            buy = plnBuy,
+            dateUpdate = date,
+            name = CURRENCY_ALIAS_ZLOTY,
+            sell = plnSell
+        )
+    ),
+    CurrencyRateSource(
+        id = branchId,
+        bankId = bankId,
+        bankName = bankName,
+        currency = Currency(
+            buy = uahBuy,
+            dateUpdate = date,
+            name = CURRENCY_ALIAS_HRYVNIA,
+            sell = uahSell
+        )
+    ),
+    CurrencyRateSource(
+        id = branchId,
+        bankId = bankId,
+        bankName = bankName,
+        currency = Currency(
+            buy = rubBuy,
+            dateUpdate = date,
+            name = CURRENCY_ALIAS_RUBLE,
+            sell = rubSell
+        )
+    )
 )
