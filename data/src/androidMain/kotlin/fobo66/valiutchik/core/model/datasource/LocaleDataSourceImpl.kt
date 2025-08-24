d1c2338dcd327a341b40c7d6d906da2af951e031
Andrei Mukamolau <4471821+fobo66@users.noreply.github.com>
2025-08-24 22:13:06 +0300

    Draft datasource for listening to locale updates

---
+++ data/src/androidMain/kotlin/fobo66/valiutchik/core/model/datasource/LocaleDataSourceImpl.kt
@@ -0,0 +1,57 @@
+/*
+ *    Copyright 2025 Andrey Mukamolov
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package fobo66.valiutchik.core.model.datasource
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.Build
+import android.os.LocaleList
+import androidx.core.content.IntentCompat
+import java.util.Locale
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+
+class LocaleDataSourceImpl(private val context: Context) : LocaleDataSource {
+
+    private var cachedLocale: Locale? = null
+
+    override val locale: Flow<Locale> = callbackFlow {
+        val localeReceiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context, intent: Intent) {
+                if (context.packageName == intent.getStringExtra(Intent.EXTRA_PACKAGE_NAME)) {
+                    val newLocaleList = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                        IntentCompat.getParcelableExtra(
+                            intent,
+                            Intent.EXTRA_LOCALE_LIST,
+                            LocaleList::class.java
+                        ) ?: LocaleList.getEmptyLocaleList()
+                    } else {
+                        LocaleList.getEmptyLocaleList()
+                    }
+                    channel.trySend(newLocaleList.get(0) ?: Locale.getDefault())
+                }
+            }
+        }
+
+        context.registerReceiver(localeReceiver, IntentFilter(Intent.ACTION_LOCALE_CHANGED))
+
+        awaitClose { context.unregisterReceiver(localeReceiver) }
+    }
+}
