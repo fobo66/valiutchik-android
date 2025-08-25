9ac607698c7f7ea2ec1c972ebfe5ed21b10a754e
Andrei Mukamolau <4471821+fobo66@users.noreply.github.com>
2025-08-25 19:53:52 +0300

    Initialized tile service

---
+++ wear/src/main/java/dev/fobo66/valiutchik/wear/CurrencyRateTileService.kt
@@ -0,0 +1,35 @@
+/*
+ *    Copyright 2025 Andrey Mukamolov
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package dev.fobo66.valiutchik.wear
+
+import androidx.wear.protolayout.ResourceBuilders
+import androidx.wear.tiles.RequestBuilders
+import androidx.wear.tiles.TileBuilders
+import com.google.android.horologist.annotations.ExperimentalHorologistApi
+import com.google.android.horologist.tiles.SuspendingTileService
+
+@OptIn(ExperimentalHorologistApi::class)
+class CurrencyRateTileService : SuspendingTileService() {
+    override suspend fun resourcesRequest(
+        requestParams: RequestBuilders.ResourcesRequest
+    ): ResourceBuilders.Resources = ResourceBuilders.Resources.Builder().setVersion("1").build()
+
+    override suspend fun tileRequest(
+        requestParams: RequestBuilders.TileRequest
+    ): TileBuilders.Tile = TileBuilders.Tile.Builder()
+        .build()
+}
