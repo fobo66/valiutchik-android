fd536388d093b9e254f334cffe0722978cd35095
Andrei Mukamolau <4471821+fobo66@users.noreply.github.com>
2025-09-06 13:35:18 +0300

    Converted IP logic to Kotlin

--- data/src/jvmMain/kotlin/fobo66/valiutchik/core/model/datasource/LocationDataSourceIpImpl.kt
+++ data/src/jvmMain/kotlin/fobo66/valiutchik/core/model/datasource/LocationDataSourceIpImpl.kt
@@ -19,8 +19,6 @@ package fobo66.valiutchik.core.model.datasource
 import fobo66.valiutchik.core.entities.Location
 import java.net.InetAddress
 import java.net.NetworkInterface
-import java.net.UnknownHostException
-import java.util.Enumeration
 
 class LocationDataSourceIpImpl : LocationDataSource {
     override suspend fun resolveLocation(): Location {
@@ -29,51 +27,11 @@ class LocationDataSourceIpImpl : LocationDataSource {
 
     // https://stackoverflow.com/a/20418809/4606884
     private fun resolveIp(): InetAddress {
-        try {
-            var candidateAddress: InetAddress? = null
-            // Iterate all NICs (network interface cards)...
-            val ifaces: Enumeration<NetworkInterface> = NetworkInterface.getNetworkInterfaces()
-            while (ifaces.hasMoreElements()) {
-                val iface = ifaces.nextElement() as NetworkInterface
-                // Iterate all IP addresses assigned to each card...
-                val inetAddrs: Enumeration<InetAddress> = iface.getInetAddresses()
-                while (inetAddrs.hasMoreElements()) {
-                    val inetAddr = inetAddrs.nextElement() as InetAddress
-                    if (!inetAddr.isLoopbackAddress()) {
-                        if (inetAddr.isSiteLocalAddress()) {
-                            // Found non-loopback site-local address. Return it immediately...
-                            return inetAddr
-                        } else if (candidateAddress == null) {
-                            // Found non-loopback address, but not necessarily site-local.
-                            // Store it as a candidate to be returned if site-local address is not subsequently found...
-                            candidateAddress = inetAddr
-                            // Note that we don't repeatedly assign non-loopback non-site-local addresses as candidates,
-                            // only the first. For subsequent iterations, candidate will be non-null.
-                        }
-                    }
-                }
-            }
-            if (candidateAddress != null) {
-                // We did not find a site-local address, but we found some other non-loopback address.
-                // Server might have a non-site-local address assigned to its NIC (or it might be running
-                // IPv6 which deprecates the "site-local" concept).
-                // Return this non-loopback candidate address...
-                return candidateAddress
-            }
-            // At this point, we did not find a non-loopback address.
-            // Fall back to returning whatever InetAddress.getLocalHost() returns...
-            val jdkSuppliedAddress = InetAddress.getLocalHost()
-            if (jdkSuppliedAddress == null) {
-                throw UnknownHostException(
-                    "The JDK InetAddress.getLocalHost() method unexpectedly returned null."
-                )
-            }
-            return jdkSuppliedAddress
-        } catch (e: Exception) {
-            val unknownHostException: UnknownHostException =
-                UnknownHostException("Failed to determine LAN address: " + e)
-            unknownHostException.initCause(e)
-            throw unknownHostException
-        }
+        val candidateAddress: InetAddress? = NetworkInterface.getNetworkInterfaces().asSequence()
+            .flatMap { it.inetAddresses.asSequence() }
+            .filterNot { it.isLoopbackAddress }
+            .lastOrNull { !it.isSiteLocalAddress }
+
+        return candidateAddress ?: InetAddress.getLocalHost()
     }
 }
