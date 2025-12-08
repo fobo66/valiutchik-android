215f926ea9ad8013f42097836d38e8d2808b34d8
Andrei Mukamolau <4471821+fobo66@users.noreply.github.com>
2025-12-09 00:10:52 +0200

    Added some KDoc comments

--- data/src/commonMain/kotlin/fobo66/valiutchik/core/model/repository/CurrencyRateRepository.kt
+++ data/src/commonMain/kotlin/fobo66/valiutchik/core/model/repository/CurrencyRateRepository.kt
@@ -33,11 +33,19 @@ interface CurrencyRateRepository {
      * Load exchange rates from database or from network
      */
     fun loadExchangeRates(): Flow<List<BestCourse>>
+
+    /**
+     * Load current user locale
+     */
     fun loadLocale(): Flow<LanguageTag>
 
     /**
      * Format currency rate into human-readable form
      */
     fun formatRate(rate: BestCourse, languageTag: LanguageTag): String
+
+    /**
+     * Simplify and transliterate bank name for display
+     */
     fun formatBankName(rate: BestCourse, languageTag: LanguageTag): String
 }
