0c1515a49de322e0b7f0c1ae9d39fec2778982bc
Andrei Mukamolau <4471821+fobo66@users.noreply.github.com>
2026-02-22 16:41:58 +0200

    Added response types for loading stuff from API

---
+++ api/src/commonMain/kotlin/fobo66/valiutchik/api/entity/CurrencyResponse.kt
@@ -0,0 +1,38 @@
+/*
+ *    Copyright 2026 Andrey Mukamolov
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package fobo66.valiutchik.api.entity
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class CurrencyResponse(
+    @SerialName("id")
+    val id: Long = 0,
+    @SerialName("iname")
+    val iname: String = "",
+    @SerialName("multiplier")
+    val multiplier: Int = 1,
+    @SerialName("name")
+    val name: String = "",
+    @SerialName("sef_alias")
+    val alias: String = "",
+    @SerialName("short_name")
+    val shortName: String = "",
+    @SerialName("symbol")
+    val symbol: String = ""
+)
