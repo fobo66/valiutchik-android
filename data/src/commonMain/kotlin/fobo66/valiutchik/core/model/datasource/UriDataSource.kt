64655891e1ff8934361b5ca8723bde9935dfd69a
Andrei Mukamolau <4471821+fobo66@users.noreply.github.com>
2026-03-11 22:45:48 +0200

    Typo

--- data/src/commonMain/kotlin/fobo66/valiutchik/core/model/datasource/UriDataSource.kt
+++ data/src/commonMain/kotlin/fobo66/valiutchik/core/model/datasource/UriDataSource.kt
@@ -1,5 +1,5 @@
 /*
- *    Copyright 2025 Andrey Mukamolov
+ *    Copyright 2026 Andrey Mukamolov
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -23,7 +23,7 @@ import com.eygraber.uri.Uri
  */
 interface UriDataSource {
     /**
-     * Forge an URI for the intent with single query parameter
+     * Forge a URI for the intent with single query parameter
      */
     fun prepareUri(query: CharSequence): Uri
 }
