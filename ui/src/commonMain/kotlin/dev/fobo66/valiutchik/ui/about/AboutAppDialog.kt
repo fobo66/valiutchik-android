91e5f204f103994d88097f23e86ca3422f3b3909
Andrei Mukamolau <4471821+fobo66@users.noreply.github.com>
2026-06-02 22:46:52 +0300

    Attempt to fix dialog behavior on web

--- ui/src/commonMain/kotlin/dev/fobo66/valiutchik/ui/about/AboutAppDialog.kt
+++ ui/src/commonMain/kotlin/dev/fobo66/valiutchik/ui/about/AboutAppDialog.kt
@@ -1,5 +1,5 @@
 /*
- *    Copyright 2025 Andrey Mukamolov
+ *    Copyright 2026 Andrey Mukamolov
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -16,16 +16,12 @@
 
 package dev.fobo66.valiutchik.ui.about
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.BasicAlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.LinkAnnotation
 import androidx.compose.ui.text.SpanStyle
@@ -38,7 +34,6 @@ import dev.fobo66.valiutchik.ui.theme.AppTheme
 import org.jetbrains.compose.resources.stringResource
 import valiutchik.ui.generated.resources.Res
 import valiutchik.ui.generated.resources.about_app_description
-import valiutchik.ui.generated.resources.action_about_close
 import valiutchik.ui.generated.resources.title_about
 
 private const val DESCRIPTION_URL = "https://myfin.by"
@@ -47,61 +42,45 @@ private const val LINK_TEXT = "myfin.by"
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AboutAppDialog(onDismiss: () -> Unit, modifier: Modifier = Modifier) {
-    BasicAlertDialog(
+    ModalBottomSheet(
+        modifier = modifier,
         onDismissRequest = onDismiss
     ) {
-        Column(
-            modifier = modifier
-                .background(
-                    color = MaterialTheme.colorScheme.surfaceContainerHigh,
-                    shape = MaterialTheme.shapes.large
-                ).padding(24.dp)
-        ) {
-            Text(
-                text = stringResource(Res.string.title_about),
-                style = MaterialTheme.typography.headlineSmall
-            )
-            val descriptionAnnotatedText =
-                buildAnnotatedString {
-                    val description = stringResource(Res.string.about_app_description)
-                    val linkIndex = description.indexOf(LINK_TEXT)
-                    val linkEndIndex = linkIndex + LINK_TEXT.length
-                    append(description)
-                    addLink(
-                        LinkAnnotation.Url(
-                            url = DESCRIPTION_URL,
-                            styles =
-                                TextLinkStyles(
-                                    style =
-                                        SpanStyle(
-                                            color = MaterialTheme.colorScheme.primary,
-                                            textDecoration = TextDecoration.Underline
-                                        )
-                                )
-                        ),
-                        linkIndex,
-                        linkEndIndex
-                    )
-                }
+        Text(
+            text = stringResource(Res.string.title_about),
+            style = MaterialTheme.typography.headlineSmall,
+            modifier = Modifier.padding(16.dp)
+        )
 
-            Text(
-                text = descriptionAnnotatedText,
-                style = MaterialTheme.typography.bodyMedium,
-                modifier = Modifier.padding(top = 16.dp)
-            )
-            TextButton(
-                onClick = onDismiss,
-                modifier = Modifier
-                    .padding(top = 24.dp)
-                    .align(Alignment.End)
-            ) {
-                Text(text = stringResource(Res.string.action_about_close))
-            }
-        }
+        Text(
+            text = buildAnnotatedString {
+                val description = stringResource(Res.string.about_app_description)
+                val linkIndex = description.indexOf(LINK_TEXT)
+                val linkEndIndex = linkIndex + LINK_TEXT.length
+                append(description)
+                addLink(
+                    LinkAnnotation.Url(
+                        url = DESCRIPTION_URL,
+                        styles =
+                            TextLinkStyles(
+                                style =
+                                    SpanStyle(
+                                        color = MaterialTheme.colorScheme.primary,
+                                        textDecoration = TextDecoration.Underline
+                                    )
+                            )
+                    ),
+                    linkIndex,
+                    linkEndIndex
+                )
+            },
+            style = MaterialTheme.typography.bodyMedium,
+            modifier = Modifier.padding(16.dp)
+        )
     }
 }
 
-@Preview
+@Preview(showSystemUi = true)
 @Composable
 private fun AboutAppPreview() {
     AppTheme {
