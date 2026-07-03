f45ddb43f72c492cbde822145bbfe19ead8e3612
Andrei Mukamolau <4471821+fobo66@users.noreply.github.com>
2026-07-04 00:56:11 +0300

    Fixed lint setup for data module

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -19,12 +19,15 @@
 import com.android.sdklib.AndroidVersion
 import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jmailen.gradle.kotlinter.tasks.FormatTask
+import org.jmailen.gradle.kotlinter.tasks.LintTask
 
 plugins {
     alias(libs.plugins.android.library.multiplatform)
     kotlin("multiplatform")
     kotlin("plugin.serialization")
     alias(libs.plugins.detekt)
+    alias(libs.plugins.kotlinter)
     alias(libs.plugins.sqlidelight)
 }
 
@@ -156,6 +159,14 @@ kotlin {
             }
         }
     }
+}
+
+tasks.withType<LintTask> {
+    exclude { it.file.path.contains("generated") }
+}
+
+tasks.withType<FormatTask> {
+    exclude { it.file.path.contains("generated") }
 }
 
 sqldelight {
--- data/src/commonMain/kotlin/fobo66/valiutchik/core/entities/OpenSourceLicensesItem.kt
+++ data/src/commonMain/kotlin/fobo66/valiutchik/core/entities/OpenSourceLicensesItem.kt
@@ -1,5 +1,5 @@
 /*
- *    Copyright 2025 Andrey Mukamolov
+ *    Copyright 2026 Andrey Mukamolov
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -23,5 +23,5 @@ data class OpenSourceLicensesItem(
     val licenses: List<String>,
     val project: String,
     val url: String?,
-    val version: String,
+    val version: String
 )
