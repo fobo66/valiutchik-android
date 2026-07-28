72f78b9353ff895eb2304f69403db620d269d264
Andrei Mukamolau <4471821+fobo66@users.noreply.github.com>
2026-07-28 21:40:07 +0300

    Organized convention plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,13 +54,6 @@ android {
         compose = true
     }
 
-    packaging {
-        resources {
-            excludes += "META-INF/AL2.0"
-            excludes += "META-INF/LGPL2.1"
-        }
-    }
-
     testOptions {
         animationsDisabled = true
         unitTests.isIncludeAndroidResources = false
--- build-logic/src/main/kotlin/AndroidLibraryConventionPlugin.kt
+++ build-logic/src/main/kotlin/AndroidLibraryConventionPlugin.kt
@@ -29,82 +29,25 @@ class AndroidLibraryConventionPlugin : Plugin<Project> {
         val hasLibraryPlugin = target.pluginManager.hasPlugin("com.android.library")
         val hasAppPlugin = target.pluginManager.hasPlugin("com.android.application")
         val hasTestPlugin = target.pluginManager.hasPlugin("com.android.test")
-        if (hasLibraryPlugin) {
-            target.extensions.configure<LibraryExtension>("android") {
-                setupSdk()
 
-                defaultConfig {
-                    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-                    consumerProguardFiles("consumer-rules.pro")
-                }
-            }
+        if (hasLibraryPlugin) {
+            target.configureLibrary()
         }
 
         if (hasTestPlugin) {
-            target.extensions.configure<TestExtension>("android") {
-                setupSdk()
-
-                defaultConfig {
-                    targetSdk {
-                        version = release(TARGET_ANDROID_SDK_VERSION)
-                    }
-
-                    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-                }
-            }
+            target.configureBaselineProfile()
         }
 
         if (hasAppPlugin) {
-            target.extensions.configure<ApplicationExtension>("android") {
-                setupSdk()
-
-                signingConfigs {
-                    register("releaseSignConfig") {
-                        val env = target.loadEnv()
-                        keyAlias = env[ENV_VAR_KEY_ALIAS]
-                        keyPassword = env[ENV_VAR_KEY_PASSWORD]
-                        storeFile = target.file(env[ENV_VAR_STORE_FILE])
-                        storePassword = env[ENV_VAR_STORE_PASSWORD]
-
-                        enableV1Signing = true
-                        enableV2Signing = true
-                        enableV3Signing = true
-                        enableV4Signing = true
-                    }
-                }
-
-                defaultConfig {
-                    targetSdk {
-                        version = release(TARGET_ANDROID_SDK_VERSION)
-                    }
-
-                    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-                }
-
-                buildTypes {
-                    release {
-                        isMinifyEnabled = true
-                        isShrinkResources = true
-                        proguardFiles(
-                            getDefaultProguardFile("proguard-android-optimize.txt"),
-                            "proguard-rules.pro"
-                        )
-                        signingConfig = signingConfigs.getByName("releaseSignConfig")
-                    }
-                }
-            }
+            target.configureApplication()
         }
 
         if (hasAppPlugin || hasLibraryPlugin || hasTestPlugin) {
-            target.extensions.configure<KotlinAndroidProjectExtension>("kotlin") {
-                compilerOptions {
-                    jvmTarget.set(JvmTarget.fromTarget(TARGET_JVM_VERSION))
-                }
-            }
+            target.configureKotlin()
         }
     }
 
-    private fun CommonExtension.setupSdk() {
+    private fun CommonExtension.configureCommon() {
         compileSdk {
             version = release(COMPILE_ANDROID_SDK_VERSION) {
                 minorApiLevel = 1
@@ -115,5 +58,76 @@ class AndroidLibraryConventionPlugin : Plugin<Project> {
         }
         compileOptions.sourceCompatibility = JavaVersion.VERSION_17
         compileOptions.targetCompatibility = JavaVersion.VERSION_17
+
+        packaging.resources.excludes.addAll(listOf("META-INF/AL2.0", "META-INF/LGPL2.1"))
     }
+
+    private fun Project.configureKotlin() =
+        extensions.configure<KotlinAndroidProjectExtension>("kotlin") {
+            compilerOptions {
+                jvmTarget.set(JvmTarget.fromTarget(TARGET_JVM_VERSION))
+            }
+        }
+
+    private fun Project.configureApplication() =
+        extensions.configure<ApplicationExtension>("android") {
+            configureCommon()
+
+            signingConfigs {
+                register("releaseSignConfig") {
+                    val env = loadEnv()
+                    keyAlias = env[ENV_VAR_KEY_ALIAS]
+                    keyPassword = env[ENV_VAR_KEY_PASSWORD]
+                    storeFile = file(env[ENV_VAR_STORE_FILE])
+                    storePassword = env[ENV_VAR_STORE_PASSWORD]
+
+                    enableV1Signing = true
+                    enableV2Signing = true
+                    enableV3Signing = true
+                    enableV4Signing = true
+                }
+            }
+
+            defaultConfig {
+                targetSdk {
+                    version = release(TARGET_ANDROID_SDK_VERSION)
+                }
+
+                testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+            }
+
+            buildTypes {
+                release {
+                    isMinifyEnabled = true
+                    isShrinkResources = true
+                    proguardFiles(
+                        getDefaultProguardFile("proguard-android-optimize.txt"),
+                        "proguard-rules.pro"
+                    )
+                    signingConfig = signingConfigs.getByName("releaseSignConfig")
+                }
+            }
+        }
+
+    private fun Project.configureLibrary() = extensions.configure<LibraryExtension>("android") {
+        configureCommon()
+
+        defaultConfig {
+            testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+            consumerProguardFiles("consumer-rules.pro")
+        }
+    }
+
+    private fun Project.configureBaselineProfile() =
+        extensions.configure<TestExtension>("android") {
+            configureCommon()
+
+            defaultConfig {
+                targetSdk {
+                    version = release(TARGET_ANDROID_SDK_VERSION)
+                }
+
+                testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+            }
+        }
 }
