fa76b39a91556bceaeab00df5c0e0bf46444e1d8
Andrei Mukamolau <4471821+fobo66@users.noreply.github.com>
2026-08-30 20:40:56 +0300

    Added appfunctions service

---
+++ app/src/main/kotlin/fobo66/exchangecourcesbelarus/mcp/ExchangeRatesAppFunctionsService.kt
@@ -0,0 +1,29 @@
+/*
+ *    Copyright 2026 Andrey Mukamolov
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package fobo66.exchangecourcesbelarus.mcp
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.appfunctions.AppFunctionService
+import androidx.appfunctions.AppFunctionServiceEntryPoint
+
+@RequiresApi(Build.VERSION_CODES.BAKLAVA)
+@AppFunctionServiceEntryPoint(
+    serviceName = "TaskAppFunctionService",
+    appFunctionXmlFileName = "task_app_function_service"
+)
+abstract class ExchangeRatesAppFunctionsService : AppFunctionService()
